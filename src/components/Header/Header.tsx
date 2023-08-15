3bf03d9044cb17157050830aeb22dd60f42f7585
Ahreum <cocoding420@gmail.com>
2023-08-15 21:15:00 +0900

    Refactor : header a->link 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,23 +2,24 @@ import React from 'react'
 import styles from './Header.module.css'
 import Logo from 'public/svgs/logoS.svg'
 import Button from '../Button/Button'
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
         <h1 className={styles.logo}>
-          <a href="/">
+          <Link href="/">
             <Logo />
-          </a>
+          </Link>
         </h1>
         <nav className={styles.nav}>
           <ul>
             <li>
-              <a href="#">프로젝트</a>
+              <Link href="#">프로젝트</Link>
             </li>
             <li>
-              <a href="#">랭킹</a>
+              <Link href="#">랭킹</Link>
             </li>
           </ul>
         </nav>

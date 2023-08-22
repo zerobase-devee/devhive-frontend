ca3ef2c5746c671029a83452f889fe9af64b3beb
Ahreum <cocoding420@gmail.com>
2023-08-23 03:26:13 +0900

    ✨ Feat: msw 실행 및 데이터 불러오기 테스트

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,12 @@ import Footer from '@/components/common/footer/Footer'
 import Header from '@/components/common/header/Header'
 import type { AppProps } from 'next/app'
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'true') {
+  import('../mocks').then(({ setupMocks }) => {
+    setupMocks()
+  })
+}
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 import Head from 'next/head'
 import styles from '@/styles/pages/home.module.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Home = () => {
+  const [data, setData] = useState('')
+
+  const getData = async () => {
+    try {
+      const response = await axios.get('/api/data')
+      setData(response.data.data)
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <>
       <Head>
@@ -12,7 +28,7 @@ const Home = () => {
       </Head>
       <div className={styles.container}>
         <div className={styles.slider}>슬라이더 영역</div>
-        <div className={styles.inner}></div>
+        <div className={styles.inner}>data: {data}</div>
       </div>
     </>
   )

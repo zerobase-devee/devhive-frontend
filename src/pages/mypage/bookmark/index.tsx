7312df11d8faabc259510955fd41c7fd230d0eda
Ahreum <cocoding420@gmail.com>
2023-08-23 10:51:48 +0900

    ✨ feat: 북마크 페이지추가

---
+++ src/pages/mypage/bookmark/index.tsx
@@ -0,0 +1,22 @@
+import Tabs from '@/components/common/tabs/Tabs'
+import Title from '@/components/common/title/Title'
+import FavoriteProjectList from '@/components/mypage/bookmark/favoriteProject/FavoriteProjectList'
+import FavoriteUserList from '@/components/mypage/bookmark/favoriteUser/FavoriteUserList'
+import MypageLayout from '@/components/mypage/common/mypageLayout/MypageLayout'
+
+const Bookmark = () => {
+  const tabMenu = ['관심프로젝트', '관심유저']
+  const tabContent = [
+    <FavoriteProjectList key={1} />,
+    <FavoriteUserList key={2} />,
+  ]
+
+  return (
+    <MypageLayout>
+      <Title title="북마크" />
+      <Tabs tabMenu={tabMenu} tabContents={tabContent} />
+    </MypageLayout>
+  )
+}
+
+export default Bookmark

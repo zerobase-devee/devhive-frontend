7860b5ce8881efb53eba93330788057b127acde3
Ahreum <cocoding420@gmail.com>
2023-08-23 11:00:23 +0900

    ✨ Feat: 내 프로필 페이지 추가

---
+++ src/pages/mypage/myprofile/index.tsx
@@ -0,0 +1,19 @@
+import styles from '@/styles/pages/myprofile.module.css'
+import Title from '@/components/common/title/Title'
+import MypageLayout from '@/components/mypage/common/mypageLayout/MypageLayout'
+import AdvancedProfile from '@/components/mypage/myprofile/advancedProfile/AdvancedProfile'
+import BasicProfile from '@/components/mypage/myprofile/basicProfile/BasicProfile'
+
+const MypageProfile = () => {
+  return (
+    <MypageLayout>
+      <Title title="내 프로필" />
+      <div className={styles.container}>
+        <BasicProfile />
+        <AdvancedProfile />
+      </div>
+    </MypageLayout>
+  )
+}
+
+export default MypageProfile

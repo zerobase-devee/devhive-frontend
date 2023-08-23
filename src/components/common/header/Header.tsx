1ce6539ca5244c2a50877d7e5e0bb30f060fd070
Ahreum <cocoding420@gmail.com>
2023-08-23 19:27:55 +0900

    ♻️ Refactor: 모달 뷰 관련 로직 수정

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,27 +1,37 @@
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import styles from './Header.module.css'
 import Logo from 'public/svgs/logoS.svg'
 import Link from 'next/link'
-import useModal from '@/hooks/useModal'
 import Button from '../button/Button'
 import LoginModal from '@/components/auth/authModal/LoginModal'
+import SignUpModal from '@/components/auth/authModal/SignupModal'
 
 const Header = () => {
   const pathname = usePathname()
-  const { openModal, handleOpenModal, handleCloseModal } = useModal()
   const router = useRouter()
+  const searchParams = useSearchParams()
 
   const handleClick = () => {
-    handleOpenModal()
+    document.body.classList.add('modalOpen')
 
     const queryString = new URLSearchParams()
     queryString.set('user', 'login')
     router.push(pathname + '?' + queryString.toString())
   }
 
+  const handleCloseModal = () => {
+    document.body.classList.remove('modalOpen')
+    router.replace(pathname)
+  }
+
   return (
     <>
-      {openModal && <LoginModal closeModal={handleCloseModal} />}
+      {searchParams.get('user') === 'signup' && (
+        <SignUpModal closeModal={handleCloseModal} />
+      )}
+      {searchParams.get('user') === 'login' && (
+        <LoginModal closeModal={handleCloseModal} />
+      )}
       <header className={styles.header}>
         <div className={styles.inner}>
           <h1 className={styles.logo}>
@@ -32,10 +42,10 @@ const Header = () => {
           <nav className={styles.nav}>
             <ul>
               <li>
-                <Link href="#">프로젝트</Link>
+                <Link href="/project">프로젝트</Link>
               </li>
               <li>
-                <Link href="#">랭킹</Link>
+                <Link href="/rank">랭킹</Link>
               </li>
             </ul>
           </nav>

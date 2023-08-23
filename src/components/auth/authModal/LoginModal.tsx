c9386d96b8e10809326c91c4b6e57401d949248a
Ahreum <cocoding420@gmail.com>
2023-08-23 19:31:13 +0900

    ♻️ Refactor: 로그인 router 추가, LinkButton->Button 변경

--- src/components/auth/authModal/LoginModal.tsx
+++ src/components/auth/authModal/LoginModal.tsx
@@ -5,39 +5,56 @@ import LoginForm from '@/components/auth/loginForm/LoginForm'
 import KakaoIcon from 'public/svgs/kakao.svg'
 import NaverIcon from 'public/svgs/naver.svg'
 import GoogleIcon from 'public/svgs/google.svg'
-import LinkButton from '@/components/common/button/LinkButton'
+import { usePathname, useRouter } from 'next/navigation'
+import Button from '@/components/common/button/Button'
 
 const LoginModal = ({ closeModal }: { closeModal: () => void }) => {
+  const router = useRouter()
+  const pathname = usePathname()
+
+  const handleSignupButtonClick = () => {
+    router.replace(pathname)
+    const queryString = new URLSearchParams()
+    queryString.set('user', 'signup')
+    router.push(pathname + '?' + queryString.toString())
+  }
+
   return (
-    <AuthModalContainer closeModal={closeModal} imgWidth={444} imgHeight={444}>
-      <div className={styles.rightSideArea}>
-        <AuthTitle text="에 로그인하세요" />
-        <>
-          <LoginForm />
-          <div className={styles.signupButtonArea}>
-            <p className={styles.text}>
-              아직 <span className={styles.bold}>devHive</span>의 회원이
-              아니신가요?
-            </p>
-            <LinkButton href="/signup" replace={true}>
-              이메일로 시작하기
-            </LinkButton>
-            <button type="button" className={styles.kakao}>
-              <KakaoIcon />
-              카카오로 시작하기
-            </button>
-            <button type="button" className={styles.naver}>
-              <NaverIcon />
-              네이버로 시작하기
-            </button>
-            <button type="button" className={styles.google}>
-              <GoogleIcon />
-              구글로 시작하기
-            </button>
-          </div>
-        </>
-      </div>
-    </AuthModalContainer>
+    <>
+      <AuthModalContainer
+        closeModal={closeModal}
+        imgWidth={444}
+        imgHeight={444}
+      >
+        <div className={styles.rightSideArea}>
+          <AuthTitle text="에 로그인하세요" />
+          <>
+            <LoginForm />
+            <div className={styles.signupButtonArea}>
+              <p className={styles.text}>
+                아직 <span className={styles.bold}>devHive</span>의 회원이
+                아니신가요?
+              </p>
+              <Button onClick={handleSignupButtonClick}>
+                이메일로 시작하기
+              </Button>
+              <button type="button" className={styles.kakao}>
+                <KakaoIcon />
+                카카오로 시작하기
+              </button>
+              <button type="button" className={styles.naver}>
+                <NaverIcon />
+                네이버로 시작하기
+              </button>
+              <button type="button" className={styles.google}>
+                <GoogleIcon />
+                구글로 시작하기
+              </button>
+            </div>
+          </>
+        </div>
+      </AuthModalContainer>
+    </>
   )
 }
 

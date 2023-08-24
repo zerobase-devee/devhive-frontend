7b340cda887eff93deb77091a1e6ae93b26c9504
Ahreum <cocoding420@gmail.com>
2023-08-25 00:58:18 +0900

    ♻️ Refactor: props 전달 함수 추가

--- src/components/mypage/myprofile/techStack/TechStackForm.tsx
+++ src/components/mypage/myprofile/techStack/TechStackForm.tsx
@@ -1,25 +1,40 @@
 import Button from '@/components/common/button/Button'
 import styles from './techStackForm.module.css'
 import TechStackSelectedBox from '@/components/techStack/techStackSelectedBox/TechStackSelectedBox'
-import { TechStackDataType } from '@/types/mypageDataType'
-import { Controller, useForm } from 'react-hook-form'
+import { useRecoilState } from 'recoil'
+import { myprofileTechState } from '@/recoil/myprofleTechStack/atom'
+import { techStackData } from 'public/data/techStackData'
 
 interface TechStackProps {
   onClose: () => void
 }
 
 const TechStackForm = ({ onClose }: TechStackProps) => {
-  const { handleSubmit, control } = useForm()
+  const [selectedItems, setSelectedItems] = useRecoilState(myprofileTechState)
 
-  const onSubmit = (data: any) => {
+  const handleItemToggle = (id: number) => {
+    setSelectedItems((prev) => {
+      if (prev.includes(id)) {
+        return prev.filter((item) => item !== id)
+      } else {
+        return [...prev, id]
+      }
+    })
+  }
+
+  const onSubmit = () => {
     onClose()
-    console.log('Form Data:', data)
+    console.log('Form Data:', selectedItems)
   }
 
   return (
-    <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
+    <form className={styles.container} onSubmit={onSubmit}>
       <div className={styles.selectBoxArea}>
-        <TechStackSelectedBox />
+        <TechStackSelectedBox
+          data={techStackData}
+          selectedItems={selectedItems}
+          handleItemToggle={handleItemToggle}
+        />
       </div>
       <div className={styles.buttonArea}>
         <Button type="button" onClick={onClose}>

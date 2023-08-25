dfabfc5e79c895dd9faa54a584aaf6bb2792cce8
Ahreum <cocoding420@gmail.com>
2023-08-25 21:25:05 +0900

    ✨ Feat: 내프로젝트 페이지, 디테일 페이지 라우팅

---
+++ src/pages/mypage/myproject/participation/[id].tsx
@@ -0,0 +1,17 @@
+import { useRouter } from 'next/router'
+import Title from '@/components/common/title/Title'
+import MypageLayout from '@/components/mypage/common/mypageLayout/MypageLayout'
+
+const ParticipationProject = () => {
+  const router = useRouter()
+  const { id } = router.query
+
+  return (
+    <MypageLayout>
+      <Title title="내 프로젝트" />
+      <div>dynamic Id : {id}</div>
+    </MypageLayout>
+  )
+}
+
+export default ParticipationProject

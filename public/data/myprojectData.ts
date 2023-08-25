b53fdecd11969e70b42bb0becb26c190e0d1f828
Ahreum <cocoding420@gmail.com>
2023-08-25 19:20:57 +0900

    📝 Docs: myprojectData 추가

---
+++ public/data/myprojectData.ts
@@ -0,0 +1,47 @@
+import { myprojectDataType } from '@/types/mypageDataType'
+
+export const myprojectPData: myprojectDataType[] = [
+  {
+    id: 1,
+    projectStatus: '팀원모집중',
+    projectTitle: '웹 개발 프로젝트 devHive',
+  },
+  {
+    id: 2,
+    projectStatus: '프로젝트시작',
+    projectTitle: '모바일 앱 개발',
+  },
+  {
+    id: 3,
+    projectStatus: '팀원재모집',
+    projectTitle: '데이터 분석 프로젝트',
+  },
+  {
+    id: 4,
+    projectStatus: '프로젝트완료',
+    projectTitle: 'UI/UX 디자인 작업',
+  },
+  {
+    id: 5,
+    projectStatus: '프로젝트시작',
+    projectTitle: '서버 구축 및 관리',
+  },
+]
+
+export const myprojectWData: myprojectDataType[] = [
+  {
+    id: 1,
+    projectStatus: '팀원모집중',
+    projectTitle: '웹 개발 프로젝트 devHive',
+  },
+  {
+    id: 2,
+    projectStatus: '프로젝트시작',
+    projectTitle: '모바일 앱 개발',
+  },
+  {
+    id: 3,
+    projectStatus: '팀원재모집',
+    projectTitle: '데이터 분석 프로젝트',
+  },
+]

ae4ed6de92d78cfb359fe793b29b8b1efc135aab
Ahreum <cocoding420@gmail.com>
2023-08-25 22:49:10 +0900

    ♻️ Refactor: 프로젝트리스트널 컴포넌트 적용

--- src/components/mypage/myproject/list/ParticipationProjectList.tsx
+++ src/components/mypage/myproject/list/ParticipationProjectList.tsx
@@ -1,8 +1,20 @@
 import { myprojectPData } from 'public/data/myprojectData'
 import ProjectList from './ProjectList'
+import ProjectListNull from './ProjectListNull'
 
 const ParticipationProjectList = () => {
-  return (
+  return myprojectPData.length === 0 ? (
+    <ProjectListNull
+      href={'/project'}
+      contentText={
+        <p>
+          아직 참여한 프로젝트가 없어요! <br />
+          프로젝트에 참여해보세요.
+        </p>
+      }
+      buttonText="프로젝트 보기"
+    />
+  ) : (
     <ProjectList
       paginationKey="participationKey"
       data={myprojectPData}
--- src/components/mypage/myproject/list/WriteProjectList.tsx
+++ src/components/mypage/myproject/list/WriteProjectList.tsx
@@ -1,8 +1,20 @@
 import { myprojectWData } from 'public/data/myprojectData'
 import ProjectList from './ProjectList'
+import ProjectListNull from './ProjectListNull'
 
 const WriteProjectList = () => {
-  return (
+  return myprojectWData.length === 0 ? (
+    <ProjectListNull
+      href={'/project'}
+      contentText={
+        <p>
+          아직 생성한 프로젝트가 없어요! <br />
+          프로젝트를 만들어보세요.
+        </p>
+      }
+      buttonText="프로젝트 올리기"
+    />
+  ) : (
     <ProjectList paginationKey="writeKey" data={myprojectWData} link="write" />
   )
 }

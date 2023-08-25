cc1a1e8692212fedb13d5d0f8498ca98e7f29b2c
Ahreum <cocoding420@gmail.com>
2023-08-25 21:18:52 +0900

    ♻️ Refactor: 로컬스토리지 키값 추가

--- src/recoil/myprofleTechStack.ts
+++ src/recoil/myprofleTechStack.ts
@@ -1,7 +1,9 @@
 import { atom } from 'recoil'
 import { recoilPersist } from 'recoil-persist'
 
-const { persistAtom } = recoilPersist()
+const { persistAtom } = recoilPersist({
+  key: 'myprofileTechState',
+})
 
 export const myprofileTechState = atom<number[]>({
   key: 'myprofileTechState',

8f67ad71a1da13578c284460f8c2c125e93fe973
Ahreum <cocoding420@gmail.com>
2023-08-27 00:09:47 +0900

    ♻️ Refactor: detailListContainer 컴포넌트 적용

--- src/pages/mypage/myproject/write/[id].tsx
+++ src/pages/mypage/myproject/write/[id].tsx
@@ -1,15 +1,12 @@
-import { useRouter } from 'next/router'
 import Title from '@/components/common/title/Title'
 import MypageLayout from '@/components/mypage/common/mypageLayout/MypageLayout'
+import DetailListContainer from '@/components/mypage/myprojectDetail/detailListContainer/DetailListContainer'
 
 const WriteProject = () => {
-  const router = useRouter()
-  const { id } = router.query
-
   return (
     <MypageLayout>
       <Title title="내 프로젝트" />
-      <div>dynamic Id : {id}</div>
+      <DetailListContainer isLeader={false} />
     </MypageLayout>
   )
 }

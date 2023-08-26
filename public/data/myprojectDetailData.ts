195a2f31deade997bf3c1b0c74f73685128fb636
Ahreum <cocoding420@gmail.com>
2023-08-27 01:07:59 +0900

    ♻️ Refactor: 데이터 구조 변경

--- public/data/myprojectDetailData.ts
+++ public/data/myprojectDetailData.ts
@@ -2,14 +2,15 @@ import { MyprojectDetailDataType } from '@/types/mypageDataType'
 
 export const myprojectDetailData: MyprojectDetailDataType[] = [
   {
+    userId: 1,
+    leader: true,
     projectId: 1,
     projectName: 'Sample Project',
-    projectStatus: '팀원모집중',
-    deadline: '2023-08-28',
+    projectStatus: '프로젝트완료',
+    deadline: '2023-01-01',
     startDate: '2023-01-01',
     endDate: '2023-12-31',
-    totalAverageScore: 4.5,
-    leader: true,
+    totalAverageScore: 21.5,
     projectMembers: [
       {
         userId: 1,
@@ -21,9 +22,18 @@ export const myprojectDetailData: MyprojectDetailDataType[] = [
         nickname: '라라라랄라',
         profileImage: '/images/techStack/javascript.png',
       },
+      {
+        userId: 3,
+        nickname: '되나요',
+        profileImage: null,
+      },
     ],
+    projectExitVote: null,
+    reviewerId: [3],
   },
   {
+    userId: 3,
+    leader: false,
     projectId: 2,
     projectName: 'Sample Project11111',
     projectStatus: '팀원모집중',
@@ -31,7 +41,6 @@ export const myprojectDetailData: MyprojectDetailDataType[] = [
     startDate: null,
     endDate: null,
     totalAverageScore: null,
-    leader: true,
     projectMembers: [
       {
         userId: 1,
@@ -43,6 +52,26 @@ export const myprojectDetailData: MyprojectDetailDataType[] = [
         nickname: '라라라랄라',
         profileImage: null,
       },
+      {
+        userId: 3,
+        nickname: '되나요',
+        profileImage: null,
+      },
     ],
+    projectExitVote: {
+      targetUserId: 3,
+      createdDate: '2023-08-26T10:15:30',
+      votedMemberList: [
+        {
+          userId: 1,
+          isVoted: true,
+        },
+        {
+          userId: 2,
+          isVoted: false,
+        },
+      ],
+    },
+    reviewerId: [],
   },
 ]

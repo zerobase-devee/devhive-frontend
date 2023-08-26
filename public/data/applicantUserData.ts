627ef12d35e81898e68a2f6763d4aa341cffd48d
Ahreum <cocoding420@gmail.com>
2023-08-27 01:44:57 +0900

    📝 Docs: 프로젝트 신청자 유저 리스트 데이터 추가

---
+++ public/data/applicantUserData.ts
@@ -0,0 +1,16 @@
+import { ApplicantUserDataType } from '@/types/mypageDataType'
+
+export const applicantUserData: ApplicantUserDataType[] = [
+  {
+    userId: 1,
+    applicationId: 1,
+    nickname: '쿨가이',
+    profileImage: '/images/techStack/javascript.png',
+  },
+  {
+    userId: 2,
+    applicationId: 3,
+    nickname: 'coolguy',
+    profileImage: null,
+  },
+]

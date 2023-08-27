098a50c7aa15569a866deb995156b1869174fd9e
Ahreum <cocoding420@gmail.com>
2023-08-28 04:10:35 +0900

    ✨ Feat: handleTechStackSave 함수 추가

--- src/hooks/useTechStack.tsx
+++ src/hooks/useTechStack.tsx
@@ -1,7 +1,20 @@
-import { useState } from 'react'
+import {
+  getSessionStorage,
+  setSessionStorage,
+} from '@/utils/saveSessionStorage'
+import { useEffect, useState } from 'react'
 
 const useTechStack = () => {
   const [selectedItems, setSelectedItems] = useState<number[]>([])
+
+  useEffect(() => {
+    const getTechStack = getSessionStorage('techStack')
+    if (!getTechStack) {
+      return setSelectedItems([])
+    } else {
+      setSelectedItems(getTechStack)
+    }
+  }, [setSelectedItems])
 
   const handleItemToggle = (id: number) => {
     setSelectedItems((prev) => {
@@ -13,7 +26,18 @@ const useTechStack = () => {
     })
   }
 
-  return { selectedItems, handleItemToggle }
+  const handleTechStackSave = (id: number) => {
+    setSelectedItems((prev) => {
+      const updatedItems = prev.includes(id)
+        ? prev.filter((item) => item !== id)
+        : [...prev, id]
+
+      setSessionStorage(updatedItems, 'techStack')
+      return updatedItems
+    })
+  }
+
+  return { selectedItems, handleItemToggle, handleTechStackSave }
 }
 
 export default useTechStack

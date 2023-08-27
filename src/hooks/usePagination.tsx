ec20b92c497df53ae9fe19baff1c6a2d983f0b79
Ahreum <cocoding420@gmail.com>
2023-08-28 02:09:57 +0900

    ♻️ Refactor: 페이지네이션저장 함수 -> 세션스토리지 유틸함수로 변경

--- src/hooks/usePagination.tsx
+++ src/hooks/usePagination.tsx
@@ -1,18 +1,21 @@
+import {
+  getSessionStorage,
+  setSessionStorage,
+} from '@/utils/saveSessionStorage'
 import { useEffect, useState } from 'react'
-import { getPagination, setPagination } from '@/utils/savePagination'
 
 const usePagination = (storageKey: string, limit: number) => {
   const [page, setPage] = useState<number>(1)
   const offset = (page - 1) * limit
 
   useEffect(() => {
-    const savedPage = getPagination(storageKey)
+    const savedPage = getSessionStorage(storageKey)
     setPage(savedPage)
   }, [storageKey])
 
   const handlePageChange = (newPage: number) => {
     setPage(newPage)
-    setPagination(newPage, storageKey)
+    setSessionStorage(newPage, storageKey)
   }
 
   return { offset, page, handlePageChange }

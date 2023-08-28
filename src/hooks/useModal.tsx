c001b360a4a67d1af724d6734ea7cad3eacc6924
Ahreum <cocoding420@gmail.com>
2023-08-28 20:38:49 +0900

    ✨ Feat : 모달 여러개 컨트롤 함수 추가

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 const useModal = () => {
+  // 모달 한개
   const [openModal, setOpenModal] = useState(false)
 
   const handleOpenModal = () => {
@@ -13,11 +14,32 @@ const useModal = () => {
     document.body.classList.remove('modalOpen')
   }
 
+  // 모달 여러개
+  const [openModals, setOpenModals] = useState<{ [name: string]: boolean }>({})
+  const handleOpenModals = (name: string) => {
+    document.body.classList.add('modalOpen')
+    setOpenModals((prev) => ({
+      ...prev,
+      [name]: true,
+    }))
+  }
+
+  const handleCloseModals = (name: string) => {
+    document.body.classList.remove('modalOpen')
+    setOpenModals((prev) => ({
+      ...prev,
+      [name]: false,
+    }))
+  }
+
   return {
     openModal,
     setOpenModal,
     handleOpenModal,
     handleCloseModal,
+    openModals,
+    handleOpenModals,
+    handleCloseModals,
   }
 }
 

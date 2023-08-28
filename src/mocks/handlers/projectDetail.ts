88357a21968d1b77b244b072fd51f129ddc0338e
Ahreum <cocoding420@gmail.com>
2023-08-28 23:06:33 +0900

    ✨ Feat : delete handler 추가

--- src/mocks/handlers/projectDetail.ts
+++ src/mocks/handlers/projectDetail.ts
@@ -1,12 +1,20 @@
 import { rest } from 'msw'
 import { projectDetailData } from 'public/data/projectDetailData'
 
-const ProjectDetail = rest.get('/api/projects/:projectId', (req, res, ctx) => {
-  const { projectId } = req.params
+const ProjectDetail = [
+  rest.get('/api/projects/:projectId', (req, res, ctx) => {
+    const { projectId } = req.params
 
-  const projectData = projectDetailData
+    const projectData = projectDetailData
 
-  return res(ctx.status(200), ctx.json(projectData))
-})
+    return res(ctx.status(200), ctx.json(projectData))
+  }),
+
+  rest.delete('/api/projects/:projectId', (req, res, ctx) => {
+    const { projectId } = req.params
+
+    return res(ctx.status(204))
+  }),
+]
 
 export default ProjectDetail

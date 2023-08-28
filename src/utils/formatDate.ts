4f080b1fac16b0ebfe9e3c608ef45257c3dbf48d
Ahreum <cocoding420@gmail.com>
2023-08-28 13:47:14 +0900

    ✨ Feat: formatDatePost 함수 추가

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -6,3 +6,24 @@ export const formatDateToYYYYMMDD = (dateTime: string) => {
 
   return `${year}-${month}-${day}`
 }
+
+export const formatDatePost = (dateTime: string) => {
+  const today = new Date().getTime()
+  const createdDate = new Date(dateTime).getTime()
+  const diff = today - createdDate
+
+  if (diff < 60 * 1000) {
+    return '방금 전'
+  } else if (diff < 60 * 60 * 1000) {
+    const minutes = Math.floor(diff / (60 * 1000))
+    return `${minutes}분 전`
+  } else if (diff < 24 * 60 * 60 * 1000) {
+    const hours = Math.floor(diff / (60 * 60 * 1000))
+    return `${hours}시간 전`
+  } else if (diff <= 7 * 24 * 60 * 60 * 1000) {
+    const days = Math.floor(diff / (24 * 60 * 60 * 1000))
+    return `${days}일 전`
+  } else {
+    return formatDateToYYYYMMDD(dateTime)
+  }
+}

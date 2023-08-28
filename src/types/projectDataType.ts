96242fc85df6d843b8a0eabf2856996a2ee54549
Ahreum <cocoding420@gmail.com>
2023-08-28 20:46:03 +0900

    📝 Docs: 프로젝트 데이터 타입 수정

--- src/types/projectDataType.ts
+++ src/types/projectDataType.ts
@@ -1,24 +1,69 @@
 export interface ParticipatingUsersDataType {
-  userId: number
-  userProfile: string | null
+  readonly userId: number
+  readonly userProfile: string | null
 }
 
-export interface ProjectCardProps {
-  projectID: number
-  title: string
-  nickname: string
-  userProfile: null | string
-  createdDate: string
-  viewCount: number
-  techStack: string[]
-  techStackImg: string[]
-  developmentType: string
-  recruitmentType: string
-  region: string | null
-  bookmark: boolean
-  participatingUsers: ParticipatingUsersDataType[]
+export interface TechStackData {
+  readonly id: number
+  readonly name: string
+  readonly image: string
+}
+
+export interface ProjectBasicData {
+  readonly projectTitle: string
+  readonly createdDate: string
+  readonly viewCount: number
+  readonly techStacks: TechStackData[]
+  readonly developmentType: string
+  readonly recruitmentType: string
+  readonly region: string | null
+  readonly bookmark: boolean
+}
+
+export interface ProjectCardProps extends ProjectBasicData {
+  readonly projectID: number
+  readonly nickname: string
+  readonly userProfile: null | string
+  readonly participatingUsers: ParticipatingUsersDataType[]
 }
 
 export interface ProjectDataType extends ProjectCardProps {
-  status: string
+  readonly status: string
+}
+
+export interface User {
+  readonly userId: number
+  readonly nickname: string
+  readonly profileImage: string | null
+}
+
+export interface Reply {
+  readonly replyId: number
+  readonly user: User
+  readonly reply: string
+  readonly createdDate: string
+  readonly modifiedDate: string | null
+}
+
+export interface Comment {
+  readonly commentId: number
+  readonly user: User
+  readonly comment: string
+  readonly createdDate: string
+  readonly modifiedDate: string | null
+  readonly replies: Reply[]
+}
+
+export interface ProjectDetailDataType extends ProjectBasicData {
+  readonly recruitmemtNum: number
+  readonly projectStatus: string
+  readonly modifiedDate: string | null
+  readonly deadline: string
+  readonly projectName: string
+  readonly content: string
+  readonly projectWriter: User
+  readonly projectMembers: User[]
+  readonly commentAndReply: Comment[]
+  readonly loginUser: User | null
+  readonly applyStatus: string | null
 }

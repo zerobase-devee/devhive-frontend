ffed621d1155f0583c4159b1d85827a0522b1178
Ahreum <cocoding420@gmail.com>
2023-08-28 20:46:52 +0900

    📝 Docs: 프로젝트 데이터 수정

--- public/data/projectCardData.ts
+++ public/data/projectCardData.ts
@@ -47,17 +47,24 @@ export const participatingUsersData2: ParticipatingUsersDataType[] = [
 export const projectCardData: ProjectDataType[] = [
   {
     projectID: 1,
-    title: '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
+    projectTitle:
+      '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
     status: '팀원모집중',
     nickname: '작성자 닉네임',
     userProfile: '/images/techStack/javascript.png',
     createdDate: '2023-08-28 12:27:30',
     viewCount: 100,
-    techStack: ['javascript', 'typescript', 'typescript'],
-    techStackImg: [
-      '/images/techStack/javascript.png',
-      '/images/techStack/typescript.png',
-      '/images/techStack/typescript.png',
+    techStacks: [
+      {
+        id: 1,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
+      {
+        id: 2,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
     ],
     developmentType: '프론트엔드',
     recruitmentType: '오프라인',
@@ -67,25 +74,24 @@ export const projectCardData: ProjectDataType[] = [
   },
   {
     projectID: 2,
-    title: '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
+    projectTitle:
+      '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
     status: '팀원재모집',
     nickname: '작성자 닉네임',
     userProfile: null,
     createdDate: '2023-08-26 15:27:30',
     viewCount: 100,
-    techStack: [
-      'javascript',
-      'typescript',
-      'typescript',
-      'typescript',
-      'typescript',
-    ],
-    techStackImg: [
-      '/images/techStack/javascript.png',
-      '/images/techStack/typescript.png',
-      '/images/techStack/typescript.png',
-      '/images/techStack/typescript.png',
-      '/images/techStack/typescript.png',
+    techStacks: [
+      {
+        id: 1,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
+      {
+        id: 2,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
     ],
     developmentType: '프론트엔드',
     recruitmentType: '온/오프라인',
@@ -95,16 +101,24 @@ export const projectCardData: ProjectDataType[] = [
   },
   {
     projectID: 3,
-    title: '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
+    projectTitle:
+      '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
     status: '프로젝트시작',
     nickname: '작성자 닉네임',
     userProfile: null,
     createdDate: '2023-08-24 15:27:30',
     viewCount: 100,
-    techStack: ['javascript', 'typescript'],
-    techStackImg: [
-      '/images/techStack/javascript.png',
-      '/images/techStack/typescript.png',
+    techStacks: [
+      {
+        id: 1,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
+      {
+        id: 2,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
     ],
     developmentType: '백엔드',
     recruitmentType: '오프라인',
@@ -114,16 +128,23 @@ export const projectCardData: ProjectDataType[] = [
   },
   {
     projectID: 4,
-    title: '프로젝트',
+    projectTitle: '프로젝트',
     status: '프로젝트시작',
     nickname: '작성자 닉네임',
     userProfile: null,
     createdDate: '2023-08-24 15:27:30',
     viewCount: 100,
-    techStack: ['javascript', 'typescript'],
-    techStackImg: [
-      '/images/techStack/javascript.png',
-      '/images/techStack/typescript.png',
+    techStacks: [
+      {
+        id: 1,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
+      {
+        id: 2,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
     ],
     developmentType: '프론트엔드',
     recruitmentType: '오프라인',
@@ -133,16 +154,24 @@ export const projectCardData: ProjectDataType[] = [
   },
   {
     projectID: 5,
-    title: '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
+    projectTitle:
+      '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
     status: '프로젝트시작',
     nickname: '작성자 닉네임',
     userProfile: null,
     createdDate: '2023-08-24 15:27:30',
     viewCount: 100,
-    techStack: ['javascript', 'typescript'],
-    techStackImg: [
-      '/images/techStack/javascript.png',
-      '/images/techStack/typescript.png',
+    techStacks: [
+      {
+        id: 1,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
+      {
+        id: 2,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
     ],
     developmentType: '풀스택',
     recruitmentType: '오프라인',
@@ -152,16 +181,24 @@ export const projectCardData: ProjectDataType[] = [
   },
   {
     projectID: 6,
-    title: '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
+    projectTitle:
+      '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
     status: '프로젝트시작',
     nickname: '작성자 닉네임',
     userProfile: null,
     createdDate: '2023-08-24 15:27:30',
     viewCount: 100,
-    techStack: ['javascript', 'typescript'],
-    techStackImg: [
-      '/images/techStack/javascript.png',
-      '/images/techStack/typescript.png',
+    techStacks: [
+      {
+        id: 1,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
+      {
+        id: 2,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
     ],
     developmentType: '프론트엔드',
     recruitmentType: '오프라인',
@@ -171,16 +208,24 @@ export const projectCardData: ProjectDataType[] = [
   },
   {
     projectID: 7,
-    title: '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
+    projectTitle:
+      '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
     status: '프로젝트시작',
     nickname: '작성자 닉네임',
     userProfile: null,
     createdDate: '2023-08-24 15:27:30',
     viewCount: 100,
-    techStack: ['javascript', 'typescript'],
-    techStackImg: [
-      '/images/techStack/javascript.png',
-      '/images/techStack/typescript.png',
+    techStacks: [
+      {
+        id: 1,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
+      {
+        id: 2,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
     ],
     developmentType: '프론트엔드',
     recruitmentType: '오프라인',
@@ -190,16 +235,24 @@ export const projectCardData: ProjectDataType[] = [
   },
   {
     projectID: 8,
-    title: '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
+    projectTitle:
+      '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
     status: '프로젝트시작',
     nickname: '작성자 닉네임',
     userProfile: null,
     createdDate: '2023-08-24 15:27:30',
     viewCount: 100,
-    techStack: ['javascript', 'typescript'],
-    techStackImg: [
-      '/images/techStack/javascript.png',
-      '/images/techStack/typescript.png',
+    techStacks: [
+      {
+        id: 1,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
+      {
+        id: 2,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
     ],
     developmentType: '프론트엔드',
     recruitmentType: '오프라인',
@@ -209,16 +262,34 @@ export const projectCardData: ProjectDataType[] = [
   },
   {
     projectID: 9,
-    title: '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
+    projectTitle:
+      '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
     status: '프로젝트시작',
     nickname: '작성자 닉네임',
     userProfile: null,
     createdDate: '2023-08-24 15:27:30',
     viewCount: 100,
-    techStack: ['javascript', 'typescript'],
-    techStackImg: [
-      '/images/techStack/javascript.png',
-      '/images/techStack/typescript.png',
+    techStacks: [
+      {
+        id: 1,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
+      {
+        id: 2,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
+      {
+        id: 3,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
+      {
+        id: 4,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
     ],
     developmentType: '프론트엔드',
     recruitmentType: '오프라인',
@@ -228,17 +299,26 @@ export const projectCardData: ProjectDataType[] = [
   },
   {
     projectID: 10,
-    title: '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
+    projectTitle:
+      '프로젝트제목목목목모곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰곰',
     status: '프로젝트시작',
     nickname: '작성자 닉네임',
     userProfile: null,
     createdDate: '2023-08-24 15:27:30',
     viewCount: 100,
-    techStack: ['javascript', 'typescript'],
-    techStackImg: [
-      '/images/techStack/javascript.png',
-      '/images/techStack/typescript.png',
+    techStacks: [
+      {
+        id: 1,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
+      {
+        id: 2,
+        name: 'javascript',
+        image: '/images/techStack/javascript.png',
+      },
     ],
+
     developmentType: '프론트엔드',
     recruitmentType: '오프라인',
     region: '부산',

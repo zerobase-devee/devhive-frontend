8f831ded2d10c4679c724539f8c18f68e0890f04
Ahreum <cocoding420@gmail.com>
2023-08-28 13:54:43 +0900

    ✨ Feat: ProjectCard 컴포넌트 추가

---
+++ src/components/project/card/ProjectCard.tsx
@@ -0,0 +1,107 @@
+import styles from './projectCard.module.css'
+import Link from 'next/link'
+import { GrView } from 'react-icons/gr'
+import { BiTime } from 'react-icons/bi'
+import { LuMoreHorizontal } from 'react-icons/lu'
+import ProjectBadge from '@/components/common/projectBadge/ProjectBadge'
+import BookmarkButton from '@/components/common/bookmarkButton/bookmarkButton'
+import Image from 'next/image'
+import UserProfileImg from '@/components/common/userProfileImg/UserProfileImg'
+import { ProjectCardProps } from '@/types/projectDataType'
+import { formatDatePost } from '@/utils/formatDate'
+
+const ProjectCard = ({
+  projectID,
+  title,
+  nickname,
+  userProfile,
+  createdDate,
+  viewCount,
+  techStack,
+  techStackImg,
+  developmentType,
+  recruitmentType,
+  region,
+  bookmark,
+  participatingUsers,
+}: ProjectCardProps) => {
+  const isRegion = () => {
+    if (region === null) {
+      return
+    } else {
+      return '・' + region
+    }
+  }
+
+  const isNewContent = (createdDate: string) => {
+    const targetTime = new Date(createdDate).getTime() + 24 * 60 * 60 * 1000
+    const currentTime = new Date().getTime()
+
+    if (currentTime <= targetTime) {
+      return <span className={styles.new}>NEW</span>
+    } else {
+      return null
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <BookmarkButton active={bookmark} />
+      <Link href={`/project/${projectID}`} className={styles.card}>
+        <div className={styles.badgeArea}>
+          <ProjectBadge>{developmentType}</ProjectBadge>
+          <ProjectBadge green>
+            {recruitmentType}
+            {isRegion()}
+          </ProjectBadge>
+        </div>
+        <div className={styles.titleArea}>
+          <p className={styles.title}>{title}</p>
+          {isNewContent(createdDate)}
+        </div>
+        <div className={styles.contentInfoArea}>
+          <div className={styles.techStackArea}>
+            {techStack.slice(0, 3).map((item, index) => (
+              <Image
+                key={item}
+                src={techStackImg[index]}
+                alt={item}
+                width={32}
+                height={32}
+              />
+            ))}
+            {techStack.length > 4 && <span>+ {techStack.length - 3}</span>}
+          </div>
+          <div className={styles.contentInfo}>
+            <BiTime />
+            <span>{formatDatePost(createdDate)}</span>
+          </div>
+          <div className={styles.contentInfo}>
+            <GrView />
+            <span>{viewCount}</span>
+          </div>
+        </div>
+        <div className={styles.userArea}>
+          <div className={styles.profile}>
+            <UserProfileImg userProfile={userProfile} width={36} height={36} />
+          </div>
+          <p className={styles.nickname}>{nickname}</p>
+          <div className={styles.participatingUsers}>
+            {participatingUsers.slice(0, 3).map((item) => (
+              <div className={styles.profileS} key={item.userId}>
+                <UserProfileImg
+                  userProfile={item.userProfile}
+                  width={28}
+                  height={28}
+                />
+              </div>
+            ))}
+            {participatingUsers.length >= 3 && <LuMoreHorizontal />}
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default ProjectCard

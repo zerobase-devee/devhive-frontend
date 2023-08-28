0a58d7a58388ab213c1671979ffce79fdf9522c6
Ahreum <cocoding420@gmail.com>
2023-08-28 15:47:06 +0900

    ✨ Feat : 프로젝트 디테일 라우팅 추가

---
+++ src/pages/project/[id].tsx
@@ -0,0 +1,12 @@
+import Container from '@/components/common/container/Container'
+import ProjectDetailContent from '@/components/projectDetail/ProjectDetailContent'
+
+const ProjectDetail = () => {
+  return (
+    <Container>
+      <ProjectDetailContent />
+    </Container>
+  )
+}
+
+export default ProjectDetail

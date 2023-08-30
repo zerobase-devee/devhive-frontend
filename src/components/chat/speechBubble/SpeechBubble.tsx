e752a0091edda18c9c4ab4cb415c70f636a2a845
Ahreum <cocoding420@gmail.com>
2023-08-31 01:44:55 +0900

    ✨ Feat : 말풍선 컴포넌트 추가

---
+++ src/components/chat/speechBubble/SpeechBubble.tsx
@@ -0,0 +1,53 @@
+import styles from './speechBubble.module.css'
+import UserProfileImg from '@/components/common/userProfileImg/UserProfileImg'
+
+interface SpeechBubbleProps {
+  received: boolean
+  message: string
+  nickname?: string
+  profileImg: string | null
+  time: string
+  checkStatus: number
+}
+
+const SpeechBubble = ({
+  received,
+  message,
+  nickname,
+  profileImg,
+  checkStatus,
+  time,
+}: SpeechBubbleProps) => {
+  return (
+    <>
+      {received && (
+        <div className={styles.user}>
+          <UserProfileImg userProfile={profileImg} width={40} height={40} />
+          <p>{nickname}</p>
+        </div>
+      )}
+      <div
+        className={`${styles.container} ${
+          received ? styles.receivedContainer : ''
+        }`}
+      >
+        <div
+          className={`${styles.status} ${
+            received ? styles.receivedStatus : ''
+          }`}
+        >
+          <p>{checkStatus}</p>
+          <p>{time}</p>
+        </div>
+
+        <div
+          className={`${styles.message} ${received ? styles.receivedMsg : ''}`}
+        >
+          <p>{message}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SpeechBubble

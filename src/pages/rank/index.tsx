c2179c060cea92c40049784cbdb50b96f0cf9438
Ahreum <cocoding420@gmail.com>
2023-08-30 19:25:33 +0900

    ♻️ Refactor : 랭킹 리스트 컴포넌트 추가

--- src/pages/rank/index.tsx
+++ src/pages/rank/index.tsx
@@ -1,10 +1,12 @@
 import Container from '@/components/common/container/Container'
 import Title from '@/components/common/title/Title'
+import RankList from '@/components/rank/list/RankList'
 
 const Rank = () => {
   return (
     <Container>
       <Title title="랭킹" />
+      <RankList />
     </Container>
   )
 }

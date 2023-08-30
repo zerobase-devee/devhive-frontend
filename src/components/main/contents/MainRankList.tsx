908aa62f068b54750be8d5ce39a06d71169d9bf4
Ahreum <cocoding420@gmail.com>
2023-08-30 22:57:51 +0900

    ♻️ Refactor : Null 컨텐츠 추가

--- src/components/main/contents/MainRankList.tsx
+++ src/components/main/contents/MainRankList.tsx
@@ -3,6 +3,7 @@ import styles from './list.module.css'
 import { RankDataType } from '@/types/rankDataType'
 import { RankData } from 'public/data/rankData'
 import RankCard from '@/components/rank/card/RankCard'
+import LinkButton from '@/components/common/button/LinkButton'
 
 const MainRankList = () => {
   const [rankData, setRankData] = useState<RankDataType[] | []>([])
@@ -11,19 +12,24 @@ const MainRankList = () => {
     setRankData(RankData)
   }, [])
 
-  return (
-    <div className={styles.list}>
-      {rankData.slice(0, 3).map((item, index) => (
-        <RankCard
-          rank={index}
-          userId={item.userId}
-          key={item.userId}
-          profileImage={item.profileImage}
-          rankPoint={item.rankPoint}
-          nickname={item.nickname}
-        />
-      ))}
-    </div>
+  return rankData.length === 0 ? (
+    <div className={styles.null}>아직 랭킹이 없어요.</div>
+  ) : (
+    <>
+      <div className={styles.list}>
+        {rankData.slice(0, 3).map((item, index) => (
+          <RankCard
+            rank={index}
+            userId={item.userId}
+            key={item.userId}
+            profileImage={item.profileImage}
+            rankPoint={item.rankPoint}
+            nickname={item.nickname}
+          />
+        ))}
+      </div>
+      <LinkButton href="/rank">랭킹 더보기</LinkButton>
+    </>
   )
 }
 

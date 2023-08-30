75ef376fe3c48759c4e0f8076026710dc1e8347e
Ahreum <cocoding420@gmail.com>
2023-08-30 22:40:10 +0900

    ✨ Feat : 캐러셀 컴포넌트 추가

---
+++ src/components/main/carousel/Carousel.tsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react'
+import styles from './carousel.module.css'
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
+import LinkButton from '@/components/common/button/LinkButton'
+import Image from 'next/image'
+import CarouselImg1 from 'public/images/carousel/carousel1.png'
+import CarouselImg2 from 'public/images/carousel/carousel2.png'
+
+const carouselContent = [
+  {
+    carouselId: 1,
+    text1: 'devHive에서',
+    text2: '원하는 프로젝트를 찾아보세요!',
+    buttonText: '프로젝트보기',
+    buttonHref: '/project',
+    imageSrc: CarouselImg1,
+    width: 303,
+    height: 303,
+    alt: '캐러셀이미지1',
+  },
+  {
+    carouselId: 2,
+    text1: 'devHive에서',
+    text2: '이번달 랭킹 1등 확인하기',
+    buttonText: '랭킹보기',
+    buttonHref: '/rank',
+    imageSrc: CarouselImg2,
+    width: 247,
+    height: 247,
+    alt: '캐러셀이미지2',
+  },
+]
+
+const Carousell = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const nextSlide = () => {
+    const newIndex = (currentIndex + 1) % carouselContent.length
+    setCurrentIndex(newIndex)
+  }
+
+  const prevSlide = () => {
+    const newIndex =
+      (currentIndex - 1 + carouselContent.length) % carouselContent.length
+    setCurrentIndex(newIndex)
+  }
+
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 3000)
+    return () => clearInterval(interval)
+  }, [currentIndex])
+
+  const content = carouselContent[currentIndex]
+
+  return (
+    <div
+      className={`${styles.container} ${
+        currentIndex === 0 ? styles.first : styles.second
+      }`}
+    >
+      <div className={styles.carousel}>
+        <div className={styles.slideItem}>
+          <div className={styles.left}>
+            <p className={styles.light}>{content.text1}</p>
+            <p className={styles.bold}>{content.text2}</p>
+            <LinkButton href={content.buttonHref} fill>
+              {content.buttonText}
+            </LinkButton>
+          </div>
+          <Image
+            src={content.imageSrc}
+            width={content.width}
+            height={content.height}
+            alt={content.alt}
+          />
+        </div>
+        <div className={styles.controller}>
+          <button className={styles.arrow} onClick={prevSlide}>
+            <IoIosArrowBack />
+          </button>
+          <div className={styles.page}>
+            <span>0{currentIndex + 1}</span>
+            <span>|</span>
+            <span>0{carouselContent.length}</span>
+          </div>
+          <button className={styles.arrow} onClick={nextSlide}>
+            <IoIosArrowForward />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Carousell

a428d34baa582a65041220a10b92a576158ba3fa
Ahreum <cocoding420@gmail.com>
2023-08-30 15:07:02 +0900

    ✨ Feat : 랭크페이지 라우터 추가

---
+++ src/pages/rank/index.tsx
@@ -0,0 +1,12 @@
+import Container from '@/components/common/container/Container'
+import Title from '@/components/common/title/Title'
+
+const Rank = () => {
+  return (
+    <Container>
+      <Title title="랭킹" />
+    </Container>
+  )
+}
+
+export default Rank

ff2a2e7483721c63f6a0bbd7bb6054d665efee82
Ahreum <cocoding420@gmail.com>
2023-09-03 01:18:04 +0900

    ✨ Feat : api 인덱스 추가

---
+++ src/pages/apis/index.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const API_BASE_URL = '/api'
+
+export const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+})

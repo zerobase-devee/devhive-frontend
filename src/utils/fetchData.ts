6b41ae1b0230a3427434528273598b8f133bbd48
Ahreum <cocoding420@gmail.com>
2023-09-04 01:47:31 +0900

    ✨ Feat : props axios 추가

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,12 +1,13 @@
-import { axiosInstance } from '@/pages/apis'
+import { AxiosInstance } from 'axios'
 import { Dispatch, SetStateAction } from 'react'
 
 export const fetchData = async (
+  axios: AxiosInstance,
   url: string,
   setData: Dispatch<SetStateAction<any>>,
 ) => {
   try {
-    const res = await axiosInstance.get(url)
+    const res = await axios.get(url)
     if (res.status === 200) {
       setData(res.data)
     }

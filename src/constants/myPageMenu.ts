9de86223481a41dea3fee3bcaca6896bc67a6ad0
Ahreum <cocoding420@gmail.com>
2023-09-04 01:51:35 +0900

    ✨ Feat : myPageMenu 상수값 추가

---
+++ src/constants/myPageMenu.ts
@@ -0,0 +1,12 @@
+export const MY_PAGE_MENU_LIST_LOCAL_LOGIN = [
+  { path: '/mypage/myprofile', label: '내 프로필' },
+  { path: '/mypage/myproject', label: '내 프로젝트' },
+  { path: '/mypage/bookmark', label: '북마크', initUrl: '?tab=관심프로젝트' },
+  { path: '/mypage/usermodify', label: '비밀번호 변경' },
+]
+
+export const MY_PAGE_MENU_LIST = [
+  { path: '/mypage/myprofile', label: '내 프로필' },
+  { path: '/mypage/myproject', label: '내 프로젝트' },
+  { path: '/mypage/bookmark', label: '북마크', initUrl: '?tab=관심프로젝트' },
+]

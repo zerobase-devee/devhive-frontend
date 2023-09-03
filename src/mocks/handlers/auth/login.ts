db092f2faa5fa02df68009849d5175a0e0301fff
Ahreum <cocoding420@gmail.com>
2023-09-03 21:34:32 +0900

    🤡 Mock: 로그인 핸들러 에러 추가

--- src/mocks/handlers/auth/login.ts
+++ src/mocks/handlers/auth/login.ts
@@ -1,13 +1,34 @@
 import { rest } from 'msw'
 
-const loginHandler = rest.post('/api/auth/signin', (_req, res, ctx) => {
-  return res(
-    ctx.status(200),
-    ctx.json({
-      accessToken: 'asdqwe123',
-      refreshToken: 'qweasd',
-    }),
-  )
-})
+type LoginRequest = {
+  email: string
+  password: string
+}
+
+const loginHandler = rest.post<LoginRequest>(
+  '/api/auth/signin',
+  (req, res, ctx) => {
+    const { email, password } = req.body
+    if (email === 'leeahreum99@naver.com' && password === 'qwer1234!') {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          accessToken: 'asdqwe123',
+          refreshToken: 'qweasd',
+        }),
+      )
+    } else {
+      return res(
+        ctx.status(400),
+        ctx.json({
+          status: 400,
+          errorCode: 'authentication_failed',
+          message:
+            '이메일 또는 비밀번호가 올바르지 않습니다. 다시 확인해주세요.',
+        }),
+      )
+    }
+  },
+)
 
 export default loginHandler

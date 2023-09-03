89b99b6f9bf903ea8677f1fc81633b39a12ac28b
Ahreum <cocoding420@gmail.com>
2023-09-03 21:32:58 +0900

    ♻️ Refactor : useLogin 훅 수정

--- src/hooks/queries/useLogin.tsx
+++ src/hooks/queries/useLogin.tsx
@@ -1,9 +1,26 @@
-import { onLogin } from '@/pages/apis/auth/loginApi'
-import { useMutation } from 'react-query'
+import { signin } from '@/pages/apis/auth/signin'
+import { loginState } from '@/recoil/loginState'
+import { LoginDataType } from '@/types/auth/loginDataType'
+import { useCookies } from 'react-cookie'
+import { useMutation, useQueryClient } from 'react-query'
+import { useSetRecoilState } from 'recoil'
 
 const useLogin = () => {
+  const [, setCookie] = useCookies()
+  const queryClient = useQueryClient()
+  const setIsLogin = useSetRecoilState(loginState)
   const loginMutation = useMutation({
-    mutationFn: (data: { email: string; password: string }) => onLogin(data),
+    mutationFn: (data: LoginDataType) => signin(data),
+    onSuccess: (authToken) => {
+      const { accessToken, refreshToken } = authToken
+      if (accessToken !== undefined && refreshToken !== undefined) {
+        queryClient.setQueryData('accessToken', accessToken)
+        queryClient.setQueryData('refreshToken', refreshToken)
+        setCookie('accessToken', authToken.accessToken)
+        setCookie('refreshToken', authToken.refreshToken)
+        setIsLogin(true)
+      }
+    },
   })
 
   return { loginMutation }

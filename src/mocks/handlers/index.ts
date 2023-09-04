6fac7771cf44fe822a53ade4e6daf5ca27eae8fa
Ahreum <cocoding420@gmail.com>
2023-09-04 16:36:57 +0900

    🤡 Mock: 리프레시 토큰 mock api 추가

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,15 +1,19 @@
 import { imageUploadHandler } from './imageUpload'
-import { userProfileHandler } from './userProfile'
+import profileHandler from './profile/profile'
 import { rankHandler } from './rank'
 import loginHandler from './auth/login'
 import ProjectDetail from './projectDetail'
 import logoutHandler from './auth/logout'
+import myprofileHandler from './mypage/myprofile'
+import refreshTokenHandler from './auth/refreshToken'
 
 export const handlers = [
   ...ProjectDetail,
   imageUploadHandler,
-  userProfileHandler,
+  profileHandler,
   rankHandler,
   loginHandler,
   logoutHandler,
+  myprofileHandler,
+  refreshTokenHandler,
 ]

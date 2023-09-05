17205df07a67d8a29e732c494e6f0cc4a2c47eea
Ahreum <cocoding420@gmail.com>
2023-09-05 15:25:59 +0900

    ✨ Feat : modal props 추가, 기본프로필 api 연결

--- src/components/mypage/myprofile/basicProfile/BasicProfile.tsx
+++ src/components/mypage/myprofile/basicProfile/BasicProfile.tsx
@@ -5,31 +5,63 @@ import Button from '@/components/common/button/Button'
 import DialogModal from '@/components/common/modal/DialogModal'
 import useModal from '@/hooks/useModal'
 import ProfileEditModal from './ProfileEditModal'
+import { useEffect, useState } from 'react'
+import { MyProfileDataType } from '@/types/mypage/myprofile'
+import { fetchData } from '@/utils/fetchData'
+import { axiosAccess } from '@/pages/apis'
+import Custom404 from '@/pages/404'
 
 const BasicProfile = () => {
   const { openModal, handleOpenModal, handleCloseModal } = useModal()
+  const [basicProfile, setBasicProfile] = useState<MyProfileDataType>()
+
+  useEffect(() => {
+    fetchData(axiosAccess, '/users/my-profile', setBasicProfile)
+  }, [])
+
+  if (!basicProfile) {
+    return <Custom404 />
+  }
 
   return (
     <>
       {openModal && (
         <DialogModal
           title="내 프로필 편집"
-          modalContent={<ProfileEditModal onClick={handleCloseModal} />}
+          modalContent={
+            <ProfileEditModal
+              nickname={basicProfile.nickname}
+              defaultImg={basicProfile.profileImage}
+              region={basicProfile.region}
+              isLocalLogin={basicProfile.isLocalLogin}
+              intro={basicProfile.intro ? basicProfile.intro : ''}
+              onClick={handleCloseModal}
+            />
+          }
           closeModal={handleCloseModal}
         />
       )}
       <div className={styles.basicProfile}>
         <div className={styles.imgArea}>
-          <UserProfileImg userProfile={null} width={140} height={140} />
+          <UserProfileImg
+            userProfile={basicProfile.profileImage}
+            width={140}
+            height={140}
+          />
         </div>
         <div className={styles.userInfoArea}>
-          <p className={styles.nickname}>닉네임 님</p>
-          <p className={styles.email}>leeahreum99@naver.com</p>
+          <p className={styles.nickname}>{basicProfile.nickname}</p>
+          <p className={styles.email}>{basicProfile.email}</p>
           <p className={styles.intro}>
-            컴포넌트에 userProfile 프로퍼티의 타입을 추가합니다.
+            {basicProfile.intro
+              ? basicProfile.intro
+              : '자기소개를 등록해주세요.'}
           </p>
           <div className={styles.badgeArea}>
-            <UserProfileBadge title="오프라인" state="미등록" />
+            <UserProfileBadge
+              title="오프라인"
+              state={`${basicProfile.region ? basicProfile.region : '미등록'}`}
+            />
             <UserProfileBadge title="벌집레벨" state="LV. 0" yellow />
             <UserProfileBadge title="퇴출전적" state="0회" red />
           </div>

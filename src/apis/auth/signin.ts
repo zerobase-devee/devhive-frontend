cdc7aad323790ef2e47203695f61b38f80ea200f
Ahreum <cocoding420@gmail.com>
2023-09-05 23:26:31 +0900

    ♻️ Refactor : throw error로 변경

--- src/apis/auth/signin.ts
+++ src/apis/auth/signin.ts
@@ -11,15 +11,6 @@ export const signin = async (formData: LoginDataType) => {
     const authToken = res.data
     return authToken
   } catch (error: any) {
-    if (error.response) {
-      const errorResponse = error.response.data
-      console.error(
-        `API 오류: ${errorResponse.errorCode} - ${errorResponse.message}`,
-      )
-      const errorMessage = errorResponse.message
-      return errorMessage
-    } else {
-      console.error('API 요청 실패', error)
-    }
+    throw error
   }
 }

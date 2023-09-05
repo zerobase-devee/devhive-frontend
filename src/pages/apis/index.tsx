88142f3a3e6b91c17a30cd2d87d84aecde84dfa3
Ahreum <cocoding420@gmail.com>
2023-09-05 17:48:40 +0900

    ♻️ Refactor : 서버 주소로 수정

--- src/pages/apis/index.tsx
+++ src/pages/apis/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { Cookies } from 'react-cookie'
 
-const API_BASE_URL = '/api'
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
 export const axiosBasic = axios.create({
   baseURL: API_BASE_URL,

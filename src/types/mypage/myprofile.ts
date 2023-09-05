f07992f59a813129b8268273c51a403c96819e12
Ahreum <cocoding420@gmail.com>
2023-09-06 01:50:54 +0900

    🐛 Fix: nickName으로 변경

--- src/types/mypage/myprofile.ts
+++ src/types/mypage/myprofile.ts
@@ -2,7 +2,7 @@ export interface MyProfileDataType {
   readonly userId: number
   readonly isLocalLogin: boolean
   readonly email: string
-  readonly nickname: string
+  readonly nickName: string
   readonly region: string | null
   readonly profileImage: string | null
   readonly intro: string | null

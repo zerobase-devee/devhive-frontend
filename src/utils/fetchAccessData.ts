2dda693b2290585c67cd499691b252acaf9ed68f
Ahreum <cocoding420@gmail.com>
2023-09-06 02:49:21 +0900

    ✨ Feat : fetchAccessData 유틸 함수 추가

---
+++ src/utils/fetchAccessData.ts
@@ -0,0 +1,21 @@
+import { axiosAccess } from '@/apis'
+import { Dispatch, SetStateAction } from 'react'
+import { Cookies } from 'react-cookie'
+
+export const fetchAccessData = async (
+  url: string,
+  setData: Dispatch<SetStateAction<any>>,
+) => {
+  const cookies = new Cookies()
+  const accessToken = cookies.get('accessToken')
+  if (accessToken !== undefined) {
+    try {
+      const res = await axiosAccess.get(url)
+      if (res.status === 200) {
+        setData(res.data)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}

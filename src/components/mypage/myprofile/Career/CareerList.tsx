e1e00b6508256b2f1c3eca6d6930967dd0fc7f54
Ahreum <cocoding420@gmail.com>
2023-09-06 17:52:54 +0900

    ♻️ Refactor : api 연결, 수정 폼 리스트로 이동

--- src/components/mypage/myprofile/Career/CareerList.tsx
+++ src/components/mypage/myprofile/Career/CareerList.tsx
@@ -1,37 +1,101 @@
 import { BiSolidPencil } from 'react-icons/bi'
 import styles from './careerList.module.css'
-
-import { CareersDataType } from '@/types/users/career'
+import { GetCareersDataType } from '@/types/users/career'
 import { formatDateToYYYYMMDD } from '@/utils/formatDate'
+import { MdDelete } from 'react-icons/md'
+import { careersDelete } from '@/apis/mypage/careersDelete'
+import React, { useEffect, useState } from 'react'
+import CareerForm from './CareerForm'
+import { useRecoilValue } from 'recoil'
+import { loginUserId } from '@/recoil/loginUserId'
+import { fetchData } from '@/utils/fetchData'
+import { useRouter } from 'next/navigation'
 
 interface CareerListProps {
-  onClick?: () => void
   view?: boolean
-  careerData: CareersDataType[]
 }
 
-const CareerList = ({ onClick, view, careerData }: CareerListProps) => {
+const CareerList = ({ view }: CareerListProps) => {
+  const router = useRouter()
+  const [editingItem, setEditingItem] = useState<number | null>(null)
+  const [careerData, setCareerData] = useState<GetCareersDataType[]>([])
+  const userId = useRecoilValue(loginUserId)
+
+  useEffect(() => {
+    if (!userId) {
+      return
+    } else {
+      fetchData(`/users/${userId}/careers`, setCareerData)
+      return
+    }
+  }, [userId])
+
+  const toggleEditing = (careerId: number) => {
+    if (editingItem === careerId) {
+      setEditingItem(null)
+    } else {
+      setEditingItem(careerId)
+    }
+  }
+  const deleteCareer = async (careerId: number) => {
+    try {
+      await careersDelete(careerId)
+      router.refresh()
+    } catch (error: any) {
+      console.error(error.response)
+    }
+  }
+
   return careerData.length === 0 ? null : (
     <div className={`${view ? styles.view : styles.list}`}>
-      {careerData.map((item: CareersDataType, index: number) => (
-        <div className={styles.item} key={index}>
-          <span className={styles.company}>{item.company}</span>
-          <span>・</span>
-          <span className={styles.position}>{item.position}</span>
-          <span>・</span>
-          <span>{formatDateToYYYYMMDD(item.startDate)}</span>
-          <span>~</span>
-          <span>
-            {item.endDate === null
-              ? '재직중'
-              : formatDateToYYYYMMDD(item.startDate)}
-          </span>
-          {!view && (
-            <button type="button" onClick={onClick}>
-              <BiSolidPencil />
-            </button>
+      {careerData.map((item: GetCareersDataType) => (
+        <React.Fragment key={item.careerId}>
+          {editingItem === item.careerId ? (
+            <CareerForm
+              careerId={item.careerId}
+              modify
+              onClose={() => toggleEditing(item.careerId)}
+              companyData={item.company}
+              positionData={item.position}
+              startDateData={formatDateToYYYYMMDD(item.startDate)}
+              endDateData={
+                item.endDate ? formatDateToYYYYMMDD(item.endDate) : ''
+              }
+            />
+          ) : (
+            <div className={styles.item} key={item.careerId}>
+              <span className={styles.company}>{item.company}</span>
+              <span>・</span>
+              <span className={styles.position}>{item.position}</span>
+              <span>・</span>
+              <span>{formatDateToYYYYMMDD(item.startDate)}</span>
+              <span>~</span>
+              <span>
+                {item.endDate === null
+                  ? '재직중'
+                  : formatDateToYYYYMMDD(item.startDate)}
+              </span>
+              {!view && (
+                <div className={styles.buttonArea}>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      deleteCareer(item.careerId)
+                    }}
+                  >
+                    <MdDelete />
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => toggleEditing(item.careerId)}
+                  >
+                    <BiSolidPencil />
+                  </button>
+                </div>
+              )}
+            </div>
           )}
-        </div>
+        </React.Fragment>
       ))}
     </div>
   )

d00a13caf73747ae49f80284a5a26012d56b480e
Ahreum <cocoding420@gmail.com>
2023-09-07 03:24:42 +0900

    ✨ Feat : image 도메인 추가

--- next.config.js
+++ next.config.js
@@ -10,18 +10,18 @@ const nextConfig = {
       },
     ]
   },
+  images: {
+    domains: ['devhive-image.s3.ap-northeast-2.amazonaws.com'],
+  },
   webpack(config) {
     if (!config.resolve.fallback) {
       config.resolve.fallback = {}
     }
-
     config.resolve.fallback.encoding = false
-
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
-
     return config
   },
 }

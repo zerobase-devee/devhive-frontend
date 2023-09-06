4baf2adfbaaad3988352687505b355ac91c094a4
Ahreum <cocoding420@gmail.com>
2023-09-06 17:53:13 +0900

    ♻️ Refactor : api 연결, props 수정

--- src/components/mypage/myprofile/Career/CareerForm.tsx
+++ src/components/mypage/myprofile/Career/CareerForm.tsx
@@ -3,7 +3,9 @@ import styles from './careerForm.module.css'
 import { useForm } from 'react-hook-form'
 import SelectedBox from '@/components/common/selectedBox/SelectedBox'
 import { useEffect, useState } from 'react'
-import { CareerDataType } from '@/types/mypageDataType'
+import { CareersDataType } from '@/types/users/career'
+import { careers } from '@/apis/mypage/careers'
+import { careersModify } from '@/apis/mypage/careersModify'
 
 interface CareerFromProps {
   onClose: () => void
@@ -12,6 +14,7 @@ interface CareerFromProps {
   startDateData?: string
   endDateData?: string
   modify?: boolean
+  careerId?: number
 }
 
 const CareerForm = ({
@@ -21,6 +24,7 @@ const CareerForm = ({
   startDateData,
   endDateData,
   modify,
+  careerId,
 }: CareerFromProps) => {
   const [isStartDateEmpty, setIsStartDateEmpty] = useState(true)
   const [isEndDateEmpty, setIsEndDateEmpty] = useState(true)
@@ -35,7 +39,7 @@ const CareerForm = ({
     reset,
     watch,
     getValues,
-  } = useForm<CareerDataType>({
+  } = useForm<CareersDataType>({
     mode: 'onChange',
     defaultValues: {
       company: companyData,
@@ -44,8 +48,6 @@ const CareerForm = ({
       endDate: endDateData,
     },
   })
-
-  console.log(selectedItem)
 
   useEffect(() => {
     if (modify) {
@@ -59,7 +61,7 @@ const CareerForm = ({
         setSelectedItem('퇴사')
       }
     }
-  }, [modify])
+  }, [modify, getValues])
 
   useEffect(() => {
     if (selectedItem === '' && !isStartDateEmpty) {
@@ -75,14 +77,37 @@ const CareerForm = ({
     }
   }, [selectedItem, isStartDateEmpty, modify, getValues, reset])
 
-  const onSubmit = (data: CareerDataType) => {
-    onClose()
-    console.log(data)
-    reset()
+  const onSubmit = async (data: CareersDataType) => {
+    try {
+      if (modify) {
+        const serverSendData: CareersDataType = {
+          company: data.company,
+          position: data.position,
+          startDate: `${data.startDate}T00:00:00`,
+          endDate: data.endDate ? `${data.endDate}T00:00:00` : null,
+        }
+        await careersModify(serverSendData, careerId ? careerId : 0)
+      } else {
+        const serverSendData: CareersDataType = {
+          company: data.company,
+          position: data.position,
+          startDate: `${data.startDate}T00:00:00`,
+          endDate: data.endDate ? `${data.endDate}T00:00:00` : '',
+        }
+        await careers(serverSendData)
+      }
+      onClose()
+      reset()
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
-    <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
+    <form
+      className={`${styles.container} ${modify && styles.modify}`}
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className={styles.inputArea}>
         <div className={styles.inputContainer}>
           <input

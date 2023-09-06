8444017fb29ea08759dc4b47fa7e6cc905138d0d
Ahreum <cocoding420@gmail.com>
2023-09-06 10:53:40 +0900

    🤡 Mock: 마이페이지 데이터 key값 수정

--- public/data/mypage/myProfileData.ts
+++ public/data/mypage/myProfileData.ts
@@ -2,9 +2,9 @@ import { MyProfileDataType } from '@/types/mypage/myprofile'
 
 export const mypageData: MyProfileDataType = {
   userId: 3,
-  isLocalLogin: true,
+  localLogin: true,
   email: 'abc@abc.com',
-  nickname: '쿨가이',
+  nickName: '쿨가이',
   region: '서울',
   profileImage: '/images/techStack/javascript.png',
   intro: '안녕하세요',

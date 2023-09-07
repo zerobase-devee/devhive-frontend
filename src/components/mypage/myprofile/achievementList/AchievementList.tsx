c95e0604a2973c5fbeb98c248406e31c7ba51233
Ahreum <cocoding420@gmail.com>
2023-09-07 23:46:22 +0900

    ✨ Feat : 뱃지 api 연결

--- src/components/mypage/myprofile/achievementList/AchievementList.tsx
+++ src/components/mypage/myprofile/achievementList/AchievementList.tsx
@@ -5,98 +5,96 @@ import deactivationAchievement2 from 'public/images/achievement/deactivationAchi
 import deactivationAchievement3 from 'public/images/achievement/deactivationAchievement3.png'
 import deactivationAchievement4 from 'public/images/achievement/deactivationAchievement4.png'
 import deactivationAchievement5 from 'public/images/achievement/deactivationAchievement5.png'
-import achievement1 from 'public/images/achievement/achievement1.png'
-import achievement2 from 'public/images/achievement/achievement2.png'
-import achievement3 from 'public/images/achievement/achievement3.png'
-import achievement4 from 'public/images/achievement/achievement4.png'
-import achievement5 from 'public/images/achievement/achievement5.png'
+import { useQuery } from 'react-query'
+import { REACT_QUERY_KEY } from '@/constants/reactQueryKey'
+import { fetchData } from '@/utils/fetchData'
+import { useRecoilValue } from 'recoil'
+import { loginUserInfo } from '@/recoil/loginUserInfo'
+import Loading from '@/components/common/loading/Loading'
+import { BadgeDataType } from '@/types/users/badgeDataType'
 
-interface AchievementListProps {
-  manner?: boolean
-  talentDonation?: boolean
-  deadline?: boolean
-  communication?: boolean
-  doctor?: boolean
-}
+const AchievementList = () => {
+  const userInfo = useRecoilValue(loginUserInfo)
+  const userId = userInfo.userId
 
-const AchievementList = ({
-  manner,
-  talentDonation,
-  deadline,
-  communication,
-  doctor,
-}: AchievementListProps) => {
+  const { data, error, isLoading } = useQuery<BadgeDataType[]>(
+    REACT_QUERY_KEY.loginUserBadge,
+    () => fetchData(`/users/${userId}/badges`),
+  )
+
+  const achievements = [
+    { src: deactivationAchievement1, text: '매너왕' },
+    { src: deactivationAchievement2, text: '재능기부왕' },
+    { src: deactivationAchievement3, text: '마감왕' },
+    { src: deactivationAchievement4, text: '소통왕' },
+    { src: deactivationAchievement5, text: '척척박사' },
+  ]
+
+  if (!data || data.length === 0) {
+    return (
+      <div className={styles.achievementList}>
+        {achievements.map((achievement, index) => (
+          <div key={index} className={styles.achievementItem}>
+            <Image
+              src={achievement.src}
+              width={48}
+              height={48}
+              alt={achievement.text}
+            />
+            <p className={styles.default}>{achievement.text}</p>
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+  if (error) {
+    return <p>에러 발생</p>
+  }
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  const MAX_EXP = 50
+
+  const experiencePoint = (score: number) => {
+    if (score % 50 === 0) {
+      return 0
+    } else if (score < 50) {
+      return score
+    } else if (score > 50) {
+      return score % 50
+    } else {
+      return score - MAX_EXP
+    }
+  }
+
+  const level = (score: number) => {
+    return Math.floor(score / 50)
+  }
+
   return (
     <div className={styles.achievementList}>
-      <div className={styles.achievementItem}>
-        {manner ? (
-          <Image src={achievement1} width={48} height={48} alt="매너왕" />
-        ) : (
+      {data.map((item: BadgeDataType) => (
+        <div key={item.badgeDto.id} className={styles.achievementItem}>
           <Image
-            src={deactivationAchievement1}
+            src={item.badgeDto.image}
             width={48}
             height={48}
-            alt="매너왕"
+            alt={item.badgeDto.name}
           />
-        )}
-        <p className={`${manner ? styles.active : styles.default}`}>매너왕</p>
-      </div>
-      <div className={styles.achievementItem}>
-        {talentDonation ? (
-          <Image src={achievement2} width={48} height={48} alt="재능기부왕" />
-        ) : (
-          <Image
-            src={deactivationAchievement2}
-            width={48}
-            height={48}
-            alt="재능기부왕"
-          />
-        )}
-        <p className={`${talentDonation ? styles.active : styles.default}`}>
-          재능기부왕
-        </p>
-      </div>
-      <div className={styles.achievementItem}>
-        {deadline ? (
-          <Image src={achievement3} width={48} height={48} alt="마감왕" />
-        ) : (
-          <Image
-            src={deactivationAchievement3}
-            width={48}
-            height={48}
-            alt="마감왕"
-          />
-        )}
-        <p className={`${deadline ? styles.active : styles.default}`}>마감왕</p>
-      </div>
-      <div className={styles.achievementItem}>
-        {communication ? (
-          <Image src={achievement4} width={48} height={48} alt="소통왕" />
-        ) : (
-          <Image
-            src={deactivationAchievement4}
-            width={48}
-            height={48}
-            alt="소통왕"
-          />
-        )}
-        <p className={`${communication ? styles.active : styles.default}`}>
-          소통왕
-        </p>
-      </div>
-      <div className={styles.achievementItem}>
-        {doctor ? (
-          <Image src={achievement5} width={48} height={48} alt="척척박사" />
-        ) : (
-          <Image
-            src={deactivationAchievement5}
-            width={48}
-            height={48}
-            alt="척척박사"
-          />
-        )}
-        <p className={`${doctor ? styles.active : styles.default}`}>척척박사</p>
-      </div>
+          <p className={styles.active}>
+            {item.badgeDto.name} Lv.{level(item.score)}
+          </p>
+          <div className={styles.levelInfo}>
+            <p>경험치</p>
+            <p className={styles.levelScore}>
+              {experiencePoint(item.score)}/{MAX_EXP}
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }

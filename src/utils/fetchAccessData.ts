59d0294bff19f8ada8dd55a94420ff0e5e8455a9
Ahreum <cocoding420@gmail.com>
2023-09-07 19:52:52 +0900

    ♻️ Refactor : 데이터 패치 함수 props 수정

--- src/utils/fetchAccessData.ts
+++ src/utils/fetchAccessData.ts
@@ -3,17 +3,20 @@ import { Dispatch, SetStateAction } from 'react'
 
 export const fetchAccessData = async (
   url: string,
-  setData: Dispatch<SetStateAction<any>>,
+  setData?: Dispatch<SetStateAction<any>>,
 ) => {
   const axiosAccess = axiosAccessFn()
 
   try {
     const res = await axiosAccess.get(url)
     if (res.status === 200) {
-      setData(res.data)
+      if (setData) {
+        setData(res.data)
+        return
+      }
+      return res.data
     }
   } catch (err) {
     console.log(err)
   }
-  return
 }
--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -3,12 +3,16 @@ import { Dispatch, SetStateAction } from 'react'
 
 export const fetchData = async (
   url: string,
-  setData: Dispatch<SetStateAction<any>>,
+  setData?: Dispatch<SetStateAction<any>>,
 ) => {
   try {
     const res = await axiosBasic.get(url)
     if (res.status === 200) {
-      setData(res.data)
+      if (setData) {
+        setData(res.data)
+        return
+      }
+      return res.data
     }
   } catch (err) {
     console.log(err)

d2bb7ce632c017b0d7f954d3e791859e0bda075c
Ahreum <cocoding420@gmail.com>
2023-09-08 13:08:58 +0900

    ✨ Feat : 랭크 데이터 타입 추가

--- src/types/rank/rankDataType.ts
+++ src/types/rank/rankDataType.ts
@@ -2,5 +2,6 @@ export interface RankDataType {
   readonly userId: number
   readonly nickName: string
   readonly profileImage: string | null
+  readonly rank: number
   readonly rankPoint: number
 }

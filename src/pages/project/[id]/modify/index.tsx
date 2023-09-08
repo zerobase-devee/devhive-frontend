295d9d9e27e90f1b0f357bae1b9df2e9e3de9db3
Ahreum <cocoding420@gmail.com>
2023-09-09 00:15:52 +0900

    ✨ Feat : 프로젝트 게시글 수정 데이터 api 추가

--- src/pages/project/[id]/modify/index.tsx
+++ src/pages/project/[id]/modify/index.tsx
@@ -1,30 +1,58 @@
 import Container from '@/components/common/container/Container'
+import Loading from '@/components/common/loading/Loading'
 import WriteForm from '@/components/projectWrite/writeForm/WriteForm'
+import { REACT_QUERY_KEY } from '@/constants/reactQueryKey'
+import useRequireLogin from '@/hooks/useRequireLogin'
+import Custom404 from '@/pages/404'
+import { ProjectDetailDataType } from '@/types/project/projectDataType'
+import { fetchAccessData } from '@/utils/fetchAccessData'
+import { formatDateToYYYYMMDD } from '@/utils/formatDate'
+import {
+  translateDevelopmentToKorean,
+  translateRecruitmentToKorean,
+} from '@/utils/projectDataToKorean'
 import { withAuthUser } from '@/utils/withAuthUser'
 import { useRouter } from 'next/router'
+import { useQuery } from 'react-query'
 
 const Modify = () => {
+  useRequireLogin()
   const router = useRouter()
   const id = Number(router.query.id)
-
-  console.log(id)
 
   const formatTeamSize = (teamSize: number) => {
     return `${teamSize}명`
   }
 
+  const { data, error, isLoading } = useQuery<ProjectDetailDataType>(
+    REACT_QUERY_KEY.projectDetail,
+    () => fetchAccessData(`/projects/${id}`),
+  )
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  if (!data) {
+    return <Custom404 />
+  }
+
+  if (error) {
+    return <Custom404 />
+  }
+
   return (
     <Container>
       <WriteForm
-        title="야호"
-        name="digh"
-        development="프론트엔드"
-        recruitment="온라인"
-        region={null}
-        content="<p>야야야양</p>"
-        teamSize={formatTeamSize(3)}
-        deadline={'2023-09-28'}
-        techStack={[1]}
+        title={data.projectTitle}
+        name={data.projectName}
+        development={translateDevelopmentToKorean(data.developmentType)}
+        recruitment={translateRecruitmentToKorean(data.recruitmentType)}
+        region={data.region}
+        content={data.content}
+        teamSize={formatTeamSize(data.recruitmentNum)}
+        deadline={formatDateToYYYYMMDD(data.deadline)}
+        techStack={data.techStacks}
         modify
       />
     </Container>

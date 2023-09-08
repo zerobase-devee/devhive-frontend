19b26b1325a0a91df65544f771f86e24164e863a
Ahreum <cocoding420@gmail.com>
2023-09-08 13:27:35 +0900

    ✨ Feat : 프로필 페이지 컨텐츠 api 연결

--- src/components/profile/ProfileContent.tsx
+++ src/components/profile/ProfileContent.tsx
@@ -1,6 +1,5 @@
 import styles from './profileContent.module.css'
 import Custom404 from '@/pages/404'
-import { UserProfileDataType } from '@/types/userProfileDataType'
 import { fetchData } from '@/utils/fetchData'
 import { useEffect, useState } from 'react'
 import UserProfileImg from '../common/userProfileImg/UserProfileImg'
@@ -10,26 +9,43 @@ import CareerList from '../mypage/myprofile/Career/CareerList'
 import ProjectHistoryItem from '../mypage/myprofile/projectHistory/ProjectHistoryItem'
 import AchievementList from '../mypage/myprofile/achievementList/AchievementList'
 import TechStackList from '../mypage/myprofile/techStack/TechStackList'
+import { ProfileDataType } from '@/types/users/profileDataType'
+import { ProjectHistoryDataType } from '@/types/users/projectHistoryDataType'
+import { useRecoilValue } from 'recoil'
+import { loginUserInfo } from '@/recoil/loginUserInfo'
+import { useQuery } from 'react-query'
+import { REACT_QUERY_KEY } from '@/constants/reactQueryKey'
+import Loading from '../common/loading/Loading'
 
-const ProfileContent = ({ userId }: { userId: number }) => {
-  const [userProfileData, setUserProfileData] =
-    useState<UserProfileDataType | null>(null)
+const ProfileContent = ({ userId }: { userId: string }) => {
+  const userInfo = useRecoilValue(loginUserInfo)
+  const loginUserId = String(userInfo.userId)
+  const { data, error, isLoading } = useQuery<ProfileDataType>(
+    REACT_QUERY_KEY.profile,
+    () => fetchData(`/users/${userId}`),
+  )
+  const [hiveLevel, setHiveLevel] = useState<string>('')
+  const [exitNum, setExitNum] = useState<string>('')
+  const [projectData, setProjectData] = useState<ProjectHistoryDataType[]>([])
+
+  console.log(projectData)
 
   useEffect(() => {
-    fetchData(`/users/${userId}`, setUserProfileData)
+    fetchData(`/members/users/${userId}/hive-level`, setHiveLevel)
+    fetchData(`/users/${userId}/exit-num`, setExitNum)
+    fetchData(`/members/users/${userId}/project-histories`, setProjectData)
   }, [userId])
 
-  if (!userProfileData) {
+  if (isLoading) {
+    return <Loading />
+  }
+
+  if (!data) {
     return <Custom404 />
   }
 
-  const achievement = () => {
-    let achievementList = []
-    for (let i = 0; i < userProfileData.badge.length; i++) {
-      achievementList.push(userProfileData.badge[i])
-    }
-
-    return achievementList
+  if (error) {
+    return <Custom404 />
   }
 
   return (
@@ -38,53 +54,55 @@ const ProfileContent = ({ userId }: { userId: number }) => {
         <div className={styles.topContent}>
           <div className={styles.profileImgBookmark}>
             <UserProfileImg
-              userProfile={userProfileData.profileImage}
+              userProfile={data.profileImage}
               width={100}
               height={100}
             />
-            <BookmarkButton active={userProfileData.isFavorite} />
+            {loginUserId === userId ? null : (
+              <BookmarkButton userId={userId} active={data.isFavorite} />
+            )}
           </div>
-          <h2 className={styles.nickname}>{userProfileData.nickname}</h2>
+          <h2 className={styles.nickname}>{data.nickName}</h2>
           <div className={styles.badgeArea}>
             <UserProfileBadge
               yellow
               title="벌집레벨"
-              state={`LV.${userProfileData.projects.length}`}
+              state={`LV.${hiveLevel}`}
             />
-            <UserProfileBadge
-              red
-              title="퇴출전적"
-              state={`${userProfileData.exitNum}회`}
-            />
+            <UserProfileBadge red title="퇴출전적" state={`${exitNum}회`} />
           </div>
-          <p className={styles.intro}>{userProfileData.intro}</p>
+          <p className={styles.intro}>{data.intro}</p>
         </div>
       </div>
       <div className={styles.bottomContainer}>
         <div className={styles.bottomItem}>
           <p className={styles.title}>경력</p>
-          <CareerList view />
+          <CareerList viewUserId={userId} view />
         </div>
         <div className={styles.bottomItem}>
           <p className={styles.title}>기술스택</p>
-          <TechStackList view />
+          <TechStackList viewUserId={userId} view />
         </div>
         <div className={styles.bottomItem}>
           <p className={styles.title}>프로젝트 참여 이력</p>
           <div className={styles.projectList}>
-            {userProfileData.projects.map((item) => (
-              <ProjectHistoryItem
-                key={item.projectName}
-                projectName={item.projectName}
-                reviewPoint={item.point}
-              />
-            ))}
+            {projectData.length === 0 ? (
+              <div className={styles.null}>아직 참여한 프로젝트가 없어요.</div>
+            ) : (
+              projectData.map((item) => (
+                <ProjectHistoryItem
+                  key={item.projectName}
+                  projectName={item.projectName}
+                  totalAverageScore={item.totalAverageScore}
+                />
+              ))
+            )}
           </div>
         </div>
         <div className={styles.bottomItem}>
           <p className={styles.title}>업적</p>
           <div className={styles.achievArea}>
-            <AchievementList />
+            <AchievementList viewUserId={userId} />
           </div>
         </div>
       </div>

d4b8fe7f4bd66ef1e00e2fe76a02c9b177d3ad23
Ahreum <cocoding420@gmail.com>
2023-09-09 04:20:32 +0900

    ✨ Feat : 프로필 데이터 타입 추가

---
+++ src/types/users/profileDataType.ts
@@ -0,0 +1,7 @@
+export interface ProfileDataType {
+  readonly userId: number
+  readonly nickName: string
+  readonly isFavorite: boolean
+  readonly profileImage: string
+  readonly intro: string
+}

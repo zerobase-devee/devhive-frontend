11248b2bb9abaf27fc2178b05751ab0855805261
Ahreum <cocoding420@gmail.com>
2023-09-08 16:44:02 +0900

    ♻️ Refactor : 데이터 타입 props 변경

--- src/components/mypage/myprofile/projectHistory/ProjectHistoryItem.tsx
+++ src/components/mypage/myprofile/projectHistory/ProjectHistoryItem.tsx
@@ -1,24 +1,17 @@
+import { ProjectHistoryDataType } from '@/types/users/projectHistoryDataType'
 import styles from './projectHistoryItem.module.css'
-import { ProjectHistoryDataType } from '@/types/projectHistoryType'
 
 const ProjectHistoryItem = ({
-  projectTitle,
-  score,
-  exclusionStatus,
+  projectName,
+  totalAverageScore,
 }: ProjectHistoryDataType) => {
   return (
     <div className={styles.projectItem}>
-      <span>{projectTitle}</span>
+      <span>{projectName}</span>
       <span>・</span>
       <span>팀원평균점수 </span>
-      <span className={styles.score}> {score} </span>
+      <span className={styles.score}> {totalAverageScore} </span>
       <span>/ 25점</span>
-      {exclusionStatus && (
-        <>
-          <span>・</span>
-          <span className={styles.exclusionStatus}>퇴출</span>
-        </>
-      )}
     </div>
   )
 }

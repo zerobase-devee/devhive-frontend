851636ca9a8a9d2cbb55154f6a05b07009a0a7d6
Ahreum <cocoding420@gmail.com>
2023-09-08 22:06:51 +0900

    ✨ Feat : projectDataToKorean 유틸함수 추가

---
+++ src/utils/projectDataToKorean.ts
@@ -0,0 +1,37 @@
+export const isRegion = (region: string | null) => {
+  if (region === null) {
+    return
+  } else {
+    return '・' + region
+  }
+}
+
+export const translateStatusToKorean = (status: string) => {
+  if (status === 'RECRUITING' || status === 'RE_RECRUITING') {
+    return '모집중'
+  } else {
+    return '모집완료'
+  }
+}
+
+export const translateRecruitmentToKorean = (recruitmentType: string) => {
+  if (recruitmentType === 'ONLINE') {
+    return '온라인'
+  } else if (recruitmentType === 'OFFLINE') {
+    return '오프라인'
+  } else {
+    return '온/오프라인'
+  }
+}
+
+export const translateDevelopmentToKorean = (developmentType: string) => {
+  if (developmentType === 'FRONTEND') {
+    return '프론트엔드'
+  } else if (developmentType === 'BACKEND') {
+    return '백엔드'
+  } else if (developmentType === 'FULLSTACK') {
+    return '풀스택'
+  } else {
+    return '전체'
+  }
+}

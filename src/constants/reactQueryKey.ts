5d5f9c6d1000cb6730159e8544b384aa3dbd1c73
Ahreum <cocoding420@gmail.com>
2023-09-08 13:07:16 +0900

    ✨ Feat : key값 추가

--- src/constants/reactQueryKey.ts
+++ src/constants/reactQueryKey.ts
@@ -2,4 +2,8 @@ export const REACT_QUERY_KEY = {
   loginUserProfile: 'loginUserProfile',
   loginUserCareer: 'loginUserCareer',
   loginUserTechStack: 'loginUserTechStack',
+  loginUserBadge: 'loginUserBadge',
+  rank: 'rank',
+  profile: 'profile',
+  favorieUser: 'favoriteUser',
 }

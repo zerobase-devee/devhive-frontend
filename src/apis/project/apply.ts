9b562cec522e5e1bb7f94682c78423ffdc855b06
Ahreum <cocoding420@gmail.com>
2023-09-09 02:18:18 +0900

    ✨ Feat : 프로젝트 신청 승인 api 추가

---
+++ src/apis/project/apply.ts
@@ -0,0 +1,53 @@
+import { axiosAccessFn } from '..'
+
+const axiosAccess = axiosAccessFn()
+
+export const postApplicationProject = async (projectId: number) => {
+  try {
+    const res = await axiosAccess({
+      method: 'post',
+      url: `/projects/${projectId}/application`,
+    })
+    return res
+  } catch (error) {
+    throw error
+  }
+}
+
+export const deleteApplicationProject = async (projectId: number) => {
+  try {
+    const res = await axiosAccess({
+      method: 'delete',
+      url: `/projects/${projectId}/application`,
+    })
+    return res
+  } catch (error) {
+    throw error
+  }
+}
+
+export const putApplicationAccept = async (applicationId: number) => {
+  try {
+    const res = await axiosAccess({
+      method: 'put',
+      url: `/projects/application/${applicationId}/accept`,
+    })
+    console.log(res)
+    return res
+  } catch (error) {
+    throw error
+  }
+}
+
+export const putApplicationReject = async (applicationId: number) => {
+  try {
+    const res = await axiosAccess({
+      method: 'put',
+      url: `/projects/application/${applicationId}/reject`,
+    })
+    console.log(res)
+    return res
+  } catch (error) {
+    throw error
+  }
+}

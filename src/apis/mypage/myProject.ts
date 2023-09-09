34c3cf6e0c8719109fcfc1c14b8ba805cdd6b2b4
Ahreum <cocoding420@gmail.com>
2023-09-10 01:13:25 +0900

    ✨ Feat : 리뷰, 투표 api 추가

--- src/apis/mypage/myProject.ts
+++ src/apis/mypage/myProject.ts
@@ -1,10 +1,14 @@
+import { reviewData } from '@/types/users/myprojectDataType'
 import { axiosAccessFn } from '..'
+import { ProjectStatus } from '@/types/project/projectDataType'
 
 const axiosAccess = axiosAccessFn()
 
 export const getProjectWrite = async (pageParam: number, pageSize: number) => {
   try {
-    const res = await axiosAccess.get(`/users/project/write`, {
+    const res = await axiosAccess({
+      method: 'get',
+      url: `/users/project/write`,
       params: {
         page: pageParam,
         size: pageSize,
@@ -21,7 +25,9 @@ export const getProjectParticipation = async (
   pageSize: number,
 ) => {
   try {
-    const res = await axiosAccess.get(`/users/project/participation`, {
+    const res = await axiosAccess({
+      method: 'get',
+      url: `/users/project/participation`,
       params: {
         page: pageParam,
         size: pageSize,
@@ -29,6 +35,77 @@ export const getProjectParticipation = async (
     })
     return res.data
   } catch (err) {
-    console.log(err)
+    console.error(err)
+  }
+}
+
+export const putProjectStauts = async (
+  projectId: number,
+  status: ProjectStatus,
+) => {
+  try {
+    const res = await axiosAccess({
+      method: 'put',
+      url: `/projects/${projectId}/status`,
+      data: status,
+    })
+
+    return res
+  } catch (error) {
+    throw error
+  }
+}
+
+export const postProjectVote = async (
+  projectId: number,
+  targetUserId: number,
+) => {
+  try {
+    const res = await axiosAccess({
+      method: 'post',
+      url: `/projects/${projectId}/vote/${targetUserId}`,
+    })
+
+    return res
+  } catch (error) {
+    throw error
+  }
+}
+
+export const putProjectVote = async (
+  projectId: number,
+  voteId: number,
+  vote: boolean,
+) => {
+  try {
+    const res = await axiosAccess({
+      method: 'put',
+      url: `/projects/${projectId}/vote/${voteId}`,
+      params: {
+        vote: vote,
+      },
+    })
+
+    return res
+  } catch (error) {
+    throw error
+  }
+}
+
+export const postProjectReview = async (
+  projectId: number,
+  targetUserId: number,
+  data: reviewData[],
+) => {
+  try {
+    const res = await axiosAccess({
+      method: 'put',
+      url: `/projects/${projectId}/review/${targetUserId}`,
+      data: data,
+    })
+
+    return res
+  } catch (error) {
+    throw error
   }
 }

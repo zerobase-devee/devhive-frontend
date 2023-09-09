49c7ae1975ee5ad2fd6957cd62c9bf54fa3749e4
Ahreum <cocoding420@gmail.com>
2023-09-09 16:54:13 +0900

    ✨ Feat : 내프로젝트 데이터 타입 추가

--- src/types/users/myprojectDataType.ts
+++ src/types/users/myprojectDataType.ts
@@ -9,3 +9,39 @@ export interface MyprojectDataType {
 export interface ProjectApplyListDataType extends UserInfo {
   readonly applicationId: number
 }
+
+export interface ProjectMemberDataType {
+  readonly userId: number
+  readonly nickName: string
+  readonly profileImage: string
+  readonly review: boolean
+}
+
+export interface MyprojectDetailDataType {
+  readonly userId: number
+  readonly leader: boolean
+  readonly projectId: number
+  readonly name: string
+  readonly status:
+    | 'RECRUITING'
+    | 'RECRUITMENT_COMPLETE'
+    | 'RE_RECRUITING'
+    | 'COMPLETE'
+
+  readonly deadline: string
+  readonly startDate: string
+  readonly endDate: string
+  readonly totalAverageScore: number
+  readonly projectMembers: ProjectMemberDataType[]
+}
+
+export interface VotedMemberDataType {
+  readonly userId: number
+  readonly isVoted: boolean
+}
+
+export interface ProjectVoteDataType {
+  readonly targetUserId: number
+  readonly createDate: string
+  readonly votedDtoList: VotedMemberDataType[]
+}

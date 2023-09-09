fdabfb8b30294aace80322bb344caba72c97660a
Ahreum <cocoding420@gmail.com>
2023-09-09 16:49:51 +0900

    ✨ Feat : comment api 추가

--- src/components/projectDetail/comment/CommentList.tsx
+++ src/components/projectDetail/comment/CommentList.tsx
@@ -1,15 +1,18 @@
 import UserProfileImg from '@/components/common/userProfileImg/UserProfileImg'
 import styles from './commentList.module.css'
-import { Comment, User } from '@/types/projectDataType'
 import { MdMoreVert } from 'react-icons/md'
 import CommentInput from './CommentInput'
 import React, { useState } from 'react'
 import Link from 'next/link'
+import { UserInfo } from '@/types/project/projectDataType'
+import { CommentDataType } from '@/types/project/commentDataType'
+import useComment from '@/hooks/queries/useComment'
+import { formatDatetoYYYYMMDDHHMM } from '@/utils/formatDate'
 
 interface CommentListProps {
-  comments: Comment[]
+  comments: CommentDataType[]
   writeUserId: number
-  loginUser: User | null
+  loginUser: UserInfo | null
 }
 
 const CommentList = ({
@@ -17,10 +20,15 @@ const CommentList = ({
   writeUserId,
   loginUser,
 }: CommentListProps) => {
+  const { deleteCommentMutation, deleteReplyMutation } = useComment()
   const [openMoreMenu, setOpenMoreMenu] = useState<{
     [commentId: number]: boolean
   }>({})
-  const [oepnReply, setOpenReply] = useState<{
+  const [openReply, setOpenReply] = useState<{
+    [commentId: number]: boolean
+  }>({})
+
+  const [isEdit, setIsEdit] = useState<{
     [commentId: number]: boolean
   }>({})
 
@@ -31,19 +39,41 @@ const CommentList = ({
     }))
   }
 
-  const handleToggleMenu = (commentId: number) => {
+  const handleToggleMenu = (id: number) => {
     setOpenMoreMenu((prev) => ({
       ...prev,
-      [commentId]: !prev[commentId],
+      [id]: !prev[id],
     }))
   }
 
-  const handleRemoveComment = () => {
-    console.log('댓글 삭제')
+  const handleModifyComment = async (id: number) => {
+    try {
+      handleToggleMenu(id)
+      setIsEdit((prev) => ({
+        ...prev,
+        [id]: !prev[id],
+      }))
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  const handleModifyComment = () => {
-    console.log('댓글 수정')
+  const handleRemoveComment = async (commentId: number) => {
+    try {
+      handleToggleMenu(commentId)
+      await deleteCommentMutation.mutateAsync(commentId)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const handleRemoveReply = async (replyId: number) => {
+    try {
+      handleToggleMenu(replyId)
+      await deleteReplyMutation.mutateAsync(replyId)
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   const countComments = () => {
@@ -63,74 +93,85 @@ const CommentList = ({
         <ul className={styles.commentsList}>
           {comments.map((item, index) => (
             <React.Fragment key={`${item.commentId}_${index}`}>
-              <li className={styles.commentArea} key={item.commentId}>
-                <div>
-                  <Link
-                    href={`/profile/@${item.user.userId}`}
-                    className={styles.user}
-                  >
-                    <UserProfileImg
-                      userProfile={item.user.profileImage}
-                      width={32}
-                      height={32}
-                    />
-                    <p className={styles.nickname}>{item.user.nickname}</p>
-                    {item.user.userId === writeUserId && (
-                      <p className={styles.writer}>작성자</p>
-                    )}
-                  </Link>
-                  <div className={styles.comment}>
-                    <p>{item.comment}</p>
-                    <div className={styles.commentInfo}>
-                      <p>{item.createdDate}</p>
-                      <button
-                        type="button"
-                        onClick={() => handleToggleReply(item.commentId)}
-                      >
-                        답글쓰기
-                      </button>
-                    </div>
-                  </div>
+              {isEdit[item.commentId] ? (
+                <div className={styles.edit}>
+                  <CommentInput
+                    edit
+                    editCommentId={item.commentId}
+                    editClick={() =>
+                      setIsEdit((prev) => ({
+                        ...prev,
+                        [item.commentId]: !prev[item.commentId],
+                      }))
+                    }
+                    editContent={item.content}
+                    loginUser={loginUser}
+                  />
                 </div>
-                {item.user.userId === writeUserId && (
-                  <div className={styles.menuContainer}>
-                    <MdMoreVert
-                      onClick={() => {
-                        handleToggleMenu(item.commentId)
-                      }}
-                    />
-                    {openMoreMenu[item.commentId] && (
-                      <div className={styles.menuList}>
+              ) : (
+                <li className={styles.commentArea} key={item.commentId}>
+                  <div>
+                    <Link
+                      href={`/profile/${item.userDto.userId}`}
+                      className={styles.user}
+                    >
+                      <UserProfileImg
+                        userProfile={item.userDto.profileImage}
+                        width={32}
+                        height={32}
+                      />
+                      <p className={styles.nickname}>{item.userDto.nickName}</p>
+                      {item.userDto.userId === writeUserId && (
+                        <p className={styles.writer}>작성자</p>
+                      )}
+                    </Link>
+                    <div className={styles.comment}>
+                      <p>{item.content}</p>
+                      <div className={styles.commentInfo}>
+                        <p>{formatDatetoYYYYMMDDHHMM(item.createDate)}</p>
                         <button
                           type="button"
-                          className={styles.buttonItem}
-                          onClick={() => {
-                            handleToggleMenu(item.commentId)
-                            handleModifyComment()
-                          }}
+                          onClick={() => handleToggleReply(item.commentId)}
                         >
-                          수정
-                        </button>
-                        <button
-                          type="button"
-                          className={styles.buttonItem}
-                          onClick={() => {
-                            handleToggleMenu(item.commentId)
-                            handleRemoveComment()
-                          }}
-                        >
-                          삭제
+                          답글쓰기
                         </button>
                       </div>
-                    )}
+                    </div>
                   </div>
-                )}
-              </li>
-              {oepnReply[item.commentId] && (
+                  {loginUser && loginUser.userId === item.userDto.userId && (
+                    <div className={styles.menuContainer}>
+                      <MdMoreVert
+                        onClick={() => {
+                          handleToggleMenu(item.commentId)
+                        }}
+                      />
+                      {openMoreMenu[item.commentId] && (
+                        <div className={styles.menuList}>
+                          <button
+                            type="button"
+                            className={styles.buttonItem}
+                            onClick={() => handleModifyComment(item.commentId)}
+                          >
+                            수정
+                          </button>
+                          <button
+                            type="button"
+                            className={styles.buttonItem}
+                            onClick={() => handleRemoveComment(item.commentId)}
+                          >
+                            삭제
+                          </button>
+                        </div>
+                      )}
+                    </div>
+                  )}
+                </li>
+              )}
+              {openReply[item.commentId] && (
                 <div className={styles.replyInputContainer}>
                   <CommentInput
                     loginUser={loginUser}
-                    onClick={handleToggleReply}
+                    handleToggleReply={() => handleToggleReply(item.commentId)}
                     commentId={item.commentId}
                   />
                 </div>
@@ -139,52 +180,85 @@ const CommentList = ({
                 <ul>
                   {item.replies.map((item, index) => (
                     <React.Fragment key={`${item.replyId}_${index}`}>
-                      <li
-                        className={`${styles.commentArea} ${styles.reply}`}
-                        key={item.replyId}
-                      >
-                        <div>
-                          <Link
-                            href={`/profile/@${item.user.userId}`}
-                            className={styles.user}
-                          >
-                            <UserProfileImg
-                              userProfile={item.user.profileImage}
-                              width={32}
-                              height={32}
-                            />
-                            <p className={styles.nickname}>
-                              {item.user.nickname}
-                            </p>
-                            {item.user.userId === writeUserId && (
-                              <p className={styles.writer}>작성자</p>
-                            )}
-                          </Link>
-                          <div className={styles.comment}>
-                            <p>{item.reply}</p>
-                            <div className={styles.commentInfo}>
-                              <p>{item.createdDate}</p>
-                              <button
-                                type="button"
-                                onClick={() =>
-                                  handleToggleReply(item.replyId * 2)
-                                }
-                              >
-                                답글쓰기
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                        {item.user.userId === writeUserId && <MdMoreVert />}
-                      </li>
-                      {oepnReply[item.replyId * 2] && (
-                        <div className={styles.replyInputContainer}>
+                      {isEdit[item.replyId] ? (
+                        <div className={styles.edit}>
                           <CommentInput
+                            edit
+                            editReplyId={item.replyId}
+                            editClick={() =>
+                              setIsEdit((prev) => ({
+                                ...prev,
+                                [item.replyId]: !prev[item.replyId],
+                              }))
+                            }
+                            editContent={item.content}
                             loginUser={loginUser}
-                            onClick={handleToggleReply}
-                            commentId={item.replyId}
                           />
                         </div>
+                      ) : (
+                        <li
+                          className={`${styles.commentArea} ${styles.reply}`}
+                          key={item.replyId}
+                        >
+                          <div>
+                            <Link
+                              href={`/profile/${item.userDto.userId}`}
+                              className={styles.user}
+                            >
+                              <UserProfileImg
+                                userProfile={item.userDto.profileImage}
+                                width={32}
+                                height={32}
+                              />
+                              <p className={styles.nickname}>
+                                {item.userDto.nickName}
+                              </p>
+                              {item.userDto.userId === writeUserId && (
+                                <p className={styles.writer}>작성자</p>
+                              )}
+                            </Link>
+                            <div className={styles.comment}>
+                              <p>{item.content}</p>
+                              <div className={styles.commentInfo}>
+                                <p>
+                                  {formatDatetoYYYYMMDDHHMM(item.createDate)}
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                          {loginUser &&
+                            loginUser.userId === item.userDto.userId && (
+                              <div className={styles.menuContainer}>
+                                <MdMoreVert
+                                  onClick={() => {
+                                    handleToggleMenu(item.replyId)
+                                  }}
+                                />
+                                {openMoreMenu[item.replyId] && (
+                                  <div className={styles.menuList}>
+                                    <button
+                                      type="button"
+                                      className={styles.buttonItem}
+                                      onClick={() =>
+                                        handleModifyComment(item.replyId)
+                                      }
+                                    >
+                                      수정
+                                    </button>
+                                    <button
+                                      type="button"
+                                      className={styles.buttonItem}
+                                      onClick={() =>
+                                        handleRemoveReply(item.replyId)
+                                      }
+                                    >
+                                      삭제
+                                    </button>
+                                  </div>
+                                )}
+                              </div>
+                            )}
+                        </li>
                       )}
                     </React.Fragment>
                   ))}

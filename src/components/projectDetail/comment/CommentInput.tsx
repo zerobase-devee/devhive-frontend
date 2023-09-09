f6d229eeb63b36f6d9a75963dce4348e8fdb4679
Ahreum <cocoding420@gmail.com>
2023-09-09 16:49:28 +0900

    ✨ Feat : commentInput api 연결

--- src/components/projectDetail/comment/CommentInput.tsx
+++ src/components/projectDetail/comment/CommentInput.tsx
@@ -1,39 +1,78 @@
+import { UserInfo } from '@/types/project/projectDataType'
 import styles from './commentInput.module.css'
 import UserProfileImg from '@/components/common/userProfileImg/UserProfileImg'
-import { User } from '@/types/projectDataType'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useForm } from 'react-hook-form'
+import useComment from '@/hooks/queries/useComment'
+import { useRouter } from 'next/router'
+import { CommentData } from '@/types/project/commentDataType'
 
 interface CommentInputProps {
-  loginUser: User | null
-  onClick?: (id: number) => void
+  loginUser: UserInfo | null
+  handleToggleReply?: () => void
   commentId?: number
+  editCommentId?: number
+  edit?: boolean
+  editContent?: string
+  editClick?: () => void
+  editReplyId?: number
 }
 
-interface CommentData {
-  comment: string
-}
-
-const CommentInput = ({ loginUser, onClick, commentId }: CommentInputProps) => {
+const CommentInput = ({
+  loginUser,
+  handleToggleReply,
+  commentId,
+  edit,
+  editContent,
+  editClick,
+  editCommentId,
+  editReplyId,
+}: CommentInputProps) => {
+  const router = useRouter()
+  const projectId = Number(router.query.id)
   const pathname = usePathname()
   const {
     register,
     handleSubmit,
-    watch,
     reset,
     formState: { errors },
-  } = useForm<CommentData>({ mode: 'onChange' })
+  } = useForm<CommentData>({
+    mode: 'onChange',
+    defaultValues: {
+      content: editContent,
+    },
+  })
 
-  const onSubmit = (data: CommentData) => {
-    if (onClick && commentId) {
-      onClick(commentId)
-    }
+  const {
+    addCommentMutation,
+    editCommentMutation,
+    addReplyMutation,
+    editReplyMutation,
+  } = useComment()
 
-    if (watch('comment')) {
-      console.log(data)
-      reset()
-      // 댓글 등록 api 연결 예정
+  const onSubmit = async (data: CommentData) => {
+    try {
+      if (handleToggleReply && commentId) {
+        // 리플
+        handleToggleReply()
+        await addReplyMutation.mutateAsync({ commentId, data })
+        reset()
+      } else if (editClick && edit && editReplyId) {
+        editClick()
+        await editReplyMutation.mutateAsync({ editReplyId, data })
+        reset()
+      } else if (edit && editCommentId && editClick) {
+        // 댓글 수정
+        editClick()
+        await editCommentMutation.mutateAsync({ editCommentId, data })
+        reset()
+      } else {
+        await addCommentMutation.mutateAsync({ projectId, data })
+        reset()
+      }
+    } catch (error) {
+      console.error(error)
     }
   }
 
@@ -54,17 +93,17 @@ const CommentInput = ({ loginUser, onClick, commentId }: CommentInputProps) => {
               width={24}
               height={24}
             />
-            <p>{loginUser.nickname}</p>
+            <p>{loginUser.nickName}</p>
           </div>
           <div className={styles.commentInputArea}>
             <textarea
               className={`${styles.commentInput} ${
-                errors.comment && styles.error
+                errors.content && styles.error
               }`}
               placeholder="댓글을 입력해주세요."
               maxLength={101}
-              aria-invalid={errors.comment ? 'true' : 'false'}
-              {...register('comment', {
+              aria-invalid={errors.content ? 'true' : 'false'}
+              {...register('content', {
                 maxLength: {
                   value: 100,
                   message: '댓글은 최대 100자까지 입력할 수 있어요.',
@@ -72,10 +111,27 @@ const CommentInput = ({ loginUser, onClick, commentId }: CommentInputProps) => {
               })}
             />
             <div>
-              {errors.comment && (
-                <p className={styles.errorMsg}>{errors.comment.message}</p>
+              {errors.content && (
+                <p className={styles.errorMsg}>{errors.content.message}</p>
               )}
-              <button type="submit">등록</button>
+              {edit ? (
+                <div className={styles.commentButtonArea}>
+                  <button
+                    className={styles.commentButton}
+                    type="button"
+                    onClick={editClick}
+                  >
+                    취소
+                  </button>
+                  <button className={styles.commentButton} type="submit">
+                    수정
+                  </button>
+                </div>
+              ) : (
+                <button className={styles.commentButton} type="submit">
+                  등록
+                </button>
+              )}
             </div>
           </div>
         </form>

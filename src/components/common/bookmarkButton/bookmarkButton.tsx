7e743ea10669d9043c36ab6a8416060abd4ce67c
Ahreum <cocoding420@gmail.com>
2023-09-10 19:57:56 +0900

    ✨ Feat : 북마크 등록 삭제 api 연결

--- src/components/common/bookmarkButton/bookmarkButton.tsx
+++ src/components/common/bookmarkButton/bookmarkButton.tsx
@@ -1,21 +1,62 @@
 import { useState } from 'react'
 import styles from './bookmarkButton.module.css'
 import { BsFillBookmarkPlusFill } from 'react-icons/bs'
+import useFavorite from '@/hooks/queries/useFavorite'
 
-const BookmarkButton = ({ active }: { active: boolean }) => {
-  const [bookmarkState, setBookMarkState] = useState(active)
+const BookmarkButton = ({
+  active,
+  userId,
+  projectId,
+  bookmarkId,
+  favoriteId,
+}: {
+  active: boolean
+  userId?: number | string
+  projectId?: number
+  bookmarkId?: number | null
+  favoriteId?: number | null
+}) => {
+  const [isFavorite, setIsFavorite] = useState(active)
+
+  const {
+    addFavoriteUserMutation,
+    deleteFavoriteUserMutation,
+    addFavoriteProjectMutation,
+    deleteFavoriteProjectMutation,
+  } = useFavorite()
+  const handleBookmark = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    try {
+      event.preventDefault()
+      setIsFavorite(!isFavorite)
+      if (active === false) {
+        if (userId) {
+          await addFavoriteUserMutation.mutateAsync(userId)
+        } else if (projectId) {
+          await addFavoriteProjectMutation.mutateAsync(projectId)
+        }
+      } else {
+        if (favoriteId) {
+          await deleteFavoriteUserMutation.mutateAsync(favoriteId)
+        } else if (bookmarkId) {
+          await deleteFavoriteProjectMutation.mutateAsync(bookmarkId)
+        } else {
+          console.log('실패')
+        }
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <button
       type="button"
       className={styles.bookmarkButton}
-      onClick={() => {
-        setBookMarkState(!bookmarkState)
-      }}
+      onClick={(event) => handleBookmark(event)}
     >
       <BsFillBookmarkPlusFill
         className={`${styles.bookmarkIcon} ${
-          bookmarkState === true && styles.favorite
+          active === true && styles.favorite
         }`}
       />
     </button>

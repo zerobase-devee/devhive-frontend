3f2bedca9e30e1c053fe76b390d7995c8c50cd08
Ahreum <cocoding420@gmail.com>
2023-09-11 02:13:50 +0900

    ✨ Feat : projectList api 추가

--- src/apis/project/projects.ts
+++ src/apis/project/projects.ts
@@ -1,7 +1,63 @@
-import { axiosAccessFn } from '..'
+import { axiosAccessFn, axiosBasic } from '..'
 import { SendProjectDataType } from '@/types/project/projectDataType'
 
 const axiosAccess = axiosAccessFn()
+
+export interface dataProps {
+  keyword: string
+  development: 'FRONTEND' | 'BACKEND' | 'FULLSTACK' | 'ALL'
+  recruitment: 'ONLINE' | 'OFFLINE' | 'ALL'
+  techStackIds: never[] | number[]
+}
+
+export const postProjectList = async (
+  pageParam: number,
+  pageSize: number,
+  data: dataProps,
+  sort: string,
+) => {
+  try {
+    const res = await axiosBasic({
+      method: 'post',
+      url: `/projects/list`,
+      params: {
+        page: pageParam,
+        size: pageSize,
+        sort: sort,
+      },
+      data: data,
+    })
+
+    return res.data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const postAccessProjectList = async (
+  pageParam: number,
+  pageSize: number,
+  data: dataProps,
+  sort: string,
+) => {
+  try {
+    const res = await axiosAccess({
+      method: 'post',
+      url: `/projects/list`,
+      params: {
+        page: pageParam,
+        size: pageSize,
+        sort: sort,
+      },
+      data: data,
+    })
+
+    console.log(data)
+    return res.data
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 export const postProject = async (formData: SendProjectDataType) => {
   try {

3840fbc2a133afc628475312f51d390b467aacb0
Ahreum <cocoding420@gmail.com>
2023-09-11 01:05:53 +0900

    🐛 Fix : page 기본값 1로 변경

--- src/hooks/usePagination.tsx
+++ src/hooks/usePagination.tsx
@@ -4,21 +4,25 @@ import {
 } from '@/utils/saveSessionStorage'
 import { useEffect, useState } from 'react'
 
-const usePagination = (storageKey: string) => {
-  const [page, setPage] = useState<number>(0)
+const usePagination = (storageKey?: string) => {
+  const [page, setPage] = useState<number>(1)
 
   useEffect(() => {
-    const savedPage = getSessionStorage(storageKey)
-    if (!savedPage) {
-      setPage(1)
-    } else {
-      setPage(savedPage)
+    if (storageKey) {
+      const savedPage = getSessionStorage(storageKey)
+      if (!savedPage) {
+        setPage(1)
+      } else {
+        setPage(savedPage)
+      }
     }
   }, [storageKey])
 
   const handlePageChange = (newPage: number) => {
     setPage(newPage)
-    setSessionStorage(newPage, storageKey)
+    if (storageKey) {
+      setSessionStorage(newPage, storageKey)
+    }
   }
 
   return { page, setPage, handlePageChange }

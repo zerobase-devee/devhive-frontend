4b14effaf8e66bc120dea1034b5a8e9eeafe8a66
Ahreum <cocoding420@gmail.com>
2023-09-10 17:11:29 +0900

    ✨ Feat : 프로젝트 북마크 api 연결

--- src/components/mypage/bookmark/favoriteProject/FavoriteProjectList.tsx
+++ src/components/mypage/bookmark/favoriteProject/FavoriteProjectList.tsx
@@ -1,27 +1,53 @@
 import styles from './favoriteProjectCard.module.css'
 import FavoriteProjectCard from './FavoriteProjectCard'
-import { FavoriteProjectListProps } from '@/types/mypageDataType'
 import FavoriteNull from '../favoriteNull/FavoriteNull'
+import usePagination from '@/hooks/usePagination'
+import { useQuery } from 'react-query'
+import { REACT_QUERY_KEY } from '@/constants/reactQueryKey'
+import { getFavoriteProject } from '@/apis/mypage/favoriteProject'
+import Loading from '@/components/common/loading/Loading'
+import { FavoriteProject } from '@/types/users/favoriteDataType'
+import Pagination from '@/components/common/pagination/Pagination'
 
 const FavoriteProjectList = () => {
-  const favoriteProjects: FavoriteProjectListProps[] = []
+  const FAVORITE_PROJECT_SIZE = 6
+  const { page, handlePageChange } = usePagination('favoriteProject')
+  const { data, error, isLoading } = useQuery(
+    [REACT_QUERY_KEY.favoriteProject, page],
+    () => getFavoriteProject(page - 1, FAVORITE_PROJECT_SIZE),
+  )
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  if (data.content.length === 0) {
+    return <FavoriteNull>북마크에 추가한 프로젝트가 아직 없어요.</FavoriteNull>
+  }
+
+  if (error) {
+    return <p>에러 발생</p>
+  }
 
   return (
-    <>
-      {favoriteProjects.length === 0 ? (
-        <FavoriteNull>북마크에 추가한 프로젝트가 아직 없어요.</FavoriteNull>
-      ) : (
-        <div className={styles.cardListContaienr}>
-          {favoriteProjects.map((project) => (
-            <FavoriteProjectCard
-              key={project.projectId}
-              projectId={project.projectId}
-              projectTitle={project.projectTitle}
-            />
-          ))}
-        </div>
-      )}
-    </>
+    <div className={styles.container}>
+      <div className={styles.cardListContaienr}>
+        {data.content.map((item: FavoriteProject) => (
+          <FavoriteProjectCard
+            bookmarkId={item.bookmarkId}
+            key={item.projectId}
+            projectId={item.projectId}
+            title={item.title}
+          />
+        ))}
+      </div>
+      <Pagination
+        page={page}
+        setPage={handlePageChange}
+        limit={FAVORITE_PROJECT_SIZE}
+        total={data.totalElements}
+      />
+    </div>
   )
 }
 

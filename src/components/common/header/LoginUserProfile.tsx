13edc2c9c61315ce2cd216c56d6f49511b4b6ce4
Ahreum <cocoding420@gmail.com>
2023-09-11 23:52:56 +0900

    ♻️ Refactor : 로그인 유저 프로필 로딩, 에러 삭제

--- src/components/common/header/LoginUserProfile.tsx
+++ src/components/common/header/LoginUserProfile.tsx
@@ -3,28 +3,25 @@ import { useQuery } from 'react-query'
 import { MyProfileDataType } from '@/types/users/myprofileDataType'
 import { REACT_QUERY_KEY } from '@/constants/reactQueryKey'
 import { fetchAccessData } from '@/utils/fetchAccessData'
-import Loading from '../loading/Loading'
 
 const LoginUserProfile = () => {
-  const { data, error, isLoading } = useQuery<MyProfileDataType>(
+  const { data } = useQuery<MyProfileDataType>(
     REACT_QUERY_KEY.loginUserProfile,
     () => fetchAccessData('/users/my-profile'),
   )
 
-  if (error) {
-    return <div>에러 발생</div>
-  }
-
-  if (isLoading) {
-    return <Loading />
-  }
-
-  if (!data) {
-    return <UserProfileImg userProfile={null} width={48} height={48} />
-  }
-
   return (
-    <UserProfileImg userProfile={data.profileImage} width={48} height={48} />
+    <>
+      {!data ? (
+        <UserProfileImg userProfile={null} width={48} height={48} />
+      ) : (
+        <UserProfileImg
+          userProfile={data.profileImage}
+          width={48}
+          height={48}
+        />
+      )}
+    </>
   )
 }
 

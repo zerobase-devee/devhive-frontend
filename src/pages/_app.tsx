fa0b18ab304e37620c4631cb581d04baf0026674
Ahreum <cocoding420@gmail.com>
2023-09-13 02:02:20 +0900

    🐛 Fix : 오타 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,7 +23,7 @@ const App = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <title>devHive</title>
-        <meta name="description" content="devHive-개발자 프로잭트 커뮤니티" />
+        <meta name="description" content="devHive-개발자 프로젝트 커뮤니티" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>

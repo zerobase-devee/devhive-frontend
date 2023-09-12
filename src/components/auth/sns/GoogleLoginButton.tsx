1dd4ee3f28f3e81abab0698749b1b16388bbe1c1
Ahreum <cocoding420@gmail.com>
2023-09-12 20:04:30 +0900

    🐛 Fix : url -> uri 변경

--- src/components/auth/sns/GoogleLoginButton.tsx
+++ src/components/auth/sns/GoogleLoginButton.tsx
@@ -4,9 +4,9 @@ import GoogleIcon from 'public/svgs/google.svg'
 
 const GoogleLoginButton = () => {
   const CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
-  const GOOGLE_REDIRECT_URL = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URL
+  const GOOGLE_REDIRECT_URI = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI
 
-  const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${GOOGLE_REDIRECT_URL}`
+  const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?response_type=token&client_id=${CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${GOOGLE_REDIRECT_URI}`
 
   return (
     <Link href={GOOGLE_AUTH_URL} className={styles.google}>
--- src/components/auth/sns/NaverLoginButton.tsx
+++ src/components/auth/sns/NaverLoginButton.tsx
@@ -4,10 +4,10 @@ import NaverIcon from 'public/svgs/naver.svg'
 
 const NaverLoginButton = () => {
   const CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID
-  const NAVER_REDIRECT_URL = process.env.NEXT_PUBLIC_NAVER_REDIRECT_URL
+  const NAVER_REDIRECT_URI = process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI
   const CLIENT_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET
 
-  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${NAVER_REDIRECT_URL}&state=${CLIENT_SECRET}`
+  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${NAVER_REDIRECT_URI}&state=${CLIENT_SECRET}`
 
   return (
     <Link href={NAVER_AUTH_URL} className={styles.naver}>

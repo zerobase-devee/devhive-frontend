8f97b3d293eaa6e2c4c81f514842bda09e247487
Ahreum <cocoding420@gmail.com>
2023-09-12 21:08:14 +0900

    🐛 Fix : 배포 CORS 설정

--- next.config.js
+++ next.config.js
@@ -2,32 +2,35 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SERVER: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
+  async headers() {
+    return [
+      {
+        source: `/api/:path*`,
+        destination: `${process.env.SERVER}/api/:path*`,
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ]
+  },
   // async rewrites() {
   //   return [
   //     {
   //       source: '/api/:path*',
   //       destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
-  //     },
-  //   ]
-  // },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: `/api/:path*`,
-  //       destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
-  //       headers: [
-  //         { key: 'Access-Control-Allow-Credentials', value: 'true' },
-  //         { key: 'Access-Control-Allow-Origin', value: '*' },
-  //         {
-  //           key: 'Access-Control-Allow-Methods',
-  //           value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-  //         },
-  //         {
-  //           key: 'Access-Control-Allow-Headers',
-  //           value:
-  //             'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-  //         },
-  //       ],
   //     },
   //   ]
   // },

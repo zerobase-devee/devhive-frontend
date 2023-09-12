1242001bc3d8d0958be70b2388c254e743b149fc
Ahreum <cocoding420@gmail.com>
2023-09-12 21:42:54 +0900

    🐛 Fix : build 오류 수정

--- next.config.js
+++ next.config.js
@@ -2,27 +2,25 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: `/api/:path*`,
-        destination: 'http://52.79.90.0:8080/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ]
-  },
+  headers: [
+    {
+      source: '/api/:path*',
+      destination: 'http://52.79.90.0:8080/api/:path*',
+      headers: [
+        { key: 'Access-Control-Allow-Credentials', value: 'true' },
+        { key: 'Access-Control-Allow-Origin', value: '*' },
+        {
+          key: 'Access-Control-Allow-Methods',
+          value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+        },
+        {
+          key: 'Access-Control-Allow-Headers',
+          value:
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+        },
+      ],
+    },
+  ],
   images: {
     domains: ['devhive-image.s3.ap-northeast-2.amazonaws.com'],
   },

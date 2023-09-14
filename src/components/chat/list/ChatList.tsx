c18c8c0f452c973810e49939b4699285b01228dc
Ahreum <cocoding420@gmail.com>
2023-09-15 02:38:58 +0900

    ✨ Feat : 채팅방 리스트 api 연결

--- src/components/chat/list/ChatList.tsx
+++ src/components/chat/list/ChatList.tsx
@@ -1,15 +1,60 @@
+import { fetchAccessData } from '@/utils/fetchAccessData'
 import styles from './chatList.module.css'
 import UserProfileImg from '@/components/common/userProfileImg/UserProfileImg'
+import { useQuery } from 'react-query'
+import { REACT_QUERY_KEY } from '@/constants/reactQueryKey'
+import Loading from '@/components/common/loading/Loading'
+import { ChatRoomDataType } from '@/types/chat/chatDataType'
+import { useRouter } from 'next/router'
+import { usePathname } from 'next/navigation'
 
 const ChatList = () => {
+  const router = useRouter()
+  const pathname = usePathname()
+
+  const { data, error, isLoading } = useQuery(REACT_QUERY_KEY.chat, () =>
+    fetchAccessData('/chat/room'),
+  )
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  if (error) {
+    return <p>에러 발생</p>
+  }
+
+  if (!data) {
+    return null
+  }
+
+  const enterChatRoom = (roomId: number) => {
+    try {
+      if (roomId) {
+        console.log('채팅방 참여')
+        const queryString = new URLSearchParams()
+        queryString.set('room', String(roomId))
+        router.push(pathname + '?' + queryString.toString())
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <ul className={styles.list}>
-      <li className={styles.item}>
-        <UserProfileImg userProfile={null} width={40} height={40} />
-        <p className={styles.chatTitle}>채팅방</p>
-        <span className={styles.team}>4</span>
-        <span className={styles.badge}>50</span>
-      </li>
+      {data.map((item: ChatRoomDataType) => (
+        <li
+          className={styles.item}
+          key={item.roomId}
+          onClick={() => enterChatRoom(item.roomId)}
+        >
+          <UserProfileImg userProfile={null} width={40} height={40} />
+          <p className={styles.chatTitle}>{item.title}</p>
+          {/* <span className={styles.team}>4</span>
+          <span className={styles.badge}>50</span> */}
+        </li>
+      ))}
     </ul>
   )
 }

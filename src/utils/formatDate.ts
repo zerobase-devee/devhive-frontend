3d91cc7b6373a2adfa8cfb4734c24a7d7897872f
Ahreum <cocoding420@gmail.com>
2023-09-15 02:40:42 +0900

    ✨ Feat : 메세지 시간 포멧 유틸함수 추가

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -71,3 +71,15 @@ export const formatDatetoHHMM = (dateTimeString: string) => {
     minute: '2-digit',
   })
 }
+
+export const formatServerSendDate = (today: Date) => {
+  const year = today.getFullYear()
+  const month = String(today.getMonth() + 1).padStart(2, '0')
+  const day = String(today.getDate()).padStart(2, '0')
+  const hours = String(today.getHours()).padStart(2, '0')
+  const minutes = String(today.getMinutes()).padStart(2, '0')
+  const seconds = String(today.getSeconds()).padStart(2, '0')
+  const milliseconds = String(today.getMilliseconds()).padStart(3, '0')
+
+  return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`
+}

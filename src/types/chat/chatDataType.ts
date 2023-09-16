1280376e28e1872d6a28a863d645566539f29c7d
Ahreum <cocoding420@gmail.com>
2023-09-16 22:58:02 +0900

    ✨ Feat : 챗 데이터 타입 추가

--- src/types/chat/chatDataType.ts
+++ src/types/chat/chatDataType.ts
@@ -1,5 +1,13 @@
+import { UserInfo } from '../project/projectDataType'
+
 export interface ChatRoomDataType {
   projectId: number
   roomId: number
   title: string
+}
+
+export interface ChatDataType {
+  userDto: UserInfo
+  text: string
+  sendTime: string
 }

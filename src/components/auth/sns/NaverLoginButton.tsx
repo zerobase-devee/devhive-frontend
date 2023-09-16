61f1a47a0157c30e4948b999fae396088ece6cf2
Ahreum <cocoding420@gmail.com>
2023-09-16 22:56:57 +0900

    🐛 Fix : sns 로그인 링크 수정

--- src/components/auth/sns/NaverLoginButton.tsx
+++ src/components/auth/sns/NaverLoginButton.tsx
@@ -3,11 +3,9 @@ import styles from './naverLoginButton.module.css'
 import NaverIcon from 'public/svgs/naver.svg'
 
 const NaverLoginButton = () => {
-  const CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID
-  const NAVER_REDIRECT_URI = process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI
-  const CLIENT_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET
-
-  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${NAVER_REDIRECT_URI}&state=${CLIENT_SECRET}`
+  const OAUTH2_REDIRECT_URI = process.env.NEXT_PUBLIC_SNS_REDIRECT_URI
+  const API_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+  const NAVER_AUTH_URL = `${API_BASE_URL}/oauth2/authorization/naver?redirect_uri=${OAUTH2_REDIRECT_URI}`
 
   return (
     <Link href={NAVER_AUTH_URL} className={styles.naver}>

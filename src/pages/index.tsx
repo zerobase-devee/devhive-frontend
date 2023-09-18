0d47996df0d8613188782157118dff84747d934c
Ahreum <cocoding420@gmail.com>
2023-09-18 22:21:51 +0900

    ✨ Feat : 닉네임 변경 안내 모달 훅, 컴포넌트 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,15 @@ import MainProjectList from '@/components/main/contents/MainProjectList'
 import MainRankList from '@/components/main/contents/MainRankList'
 import Carousel from '@/components/main/carousel/Carousel'
 import { withAuthUser } from '@/utils/withAuthUser'
+import NicknameChangeInfoModal from '@/components/auth/sns/NicknameChangeInfoModal'
+import useNicknameChangeModal from '@/hooks/useNicknameChangeModal'
 
 const Home = () => {
+  const { openModal } = useNicknameChangeModal()
+
   return (
     <>
+      {openModal && <NicknameChangeInfoModal />}
       <div className={styles.container}>
         <Carousel />
         <div className={styles.inner}>
--- src/pages/project/write/index.tsx
+++ src/pages/project/write/index.tsx
@@ -1,15 +1,21 @@
+import NicknameChangeInfoModal from '@/components/auth/sns/NicknameChangeInfoModal'
 import Container from '@/components/common/container/Container'
 import WriteForm from '@/components/projectWrite/writeForm/WriteForm'
+import useNicknameChangeModal from '@/hooks/useNicknameChangeModal'
 import useRequireLogin from '@/hooks/useRequireLogin'
 import { withAuthUser } from '@/utils/withAuthUser'
 
 const ProjectWrite = () => {
   useRequireLogin()
+  const { openModal } = useNicknameChangeModal()
 
   return (
-    <Container>
-      <WriteForm />
-    </Container>
+    <>
+      {openModal && <NicknameChangeInfoModal />}
+      <Container>
+        <WriteForm />
+      </Container>
+    </>
   )
 }
 

b75e567847e4f0d0511e83083369ce84302576ff
Ahreum <cocoding420@gmail.com>
2023-09-20 01:58:11 +0900

    ✨ Feat : 랭크 userBadges 데이터 타입 추가

--- src/types/rank/rankDataType.ts
+++ src/types/rank/rankDataType.ts
@@ -1,6 +1,9 @@
+import { BadgeDataType } from '../users/badgeDataType'
+
 export interface RankDataType {
   readonly userId: number
   readonly nickName: string
   readonly profileImage: string | null
   readonly rankPoint: number
+  readonly userBadges: BadgeDataType[]
 }

efeb7fa6e6510aa38f843eebf5af10cfd56d7d3c
Ahreum <cocoding420@gmail.com>
2023-09-22 02:02:49 +0900

    ♻️ Refactor: 알림 콘텐츠 상수파일로 분리

---
+++ src/constants/alarm.ts
@@ -0,0 +1,32 @@
+export const ALARM_CONTENT = {
+  COMMENT: '에 새로운 댓글을 확인해보세요.',
+  REPLY: '에 작성한 내 댓글에 답글이 달렸어요.',
+  PROJECT_APPLY: '에 새로운 신청자가 있어요.',
+  APPLICANT_ACCEPT: '에 신청이 수락 되었어요.',
+  APPLICANT_REJECT: '에 신청이 거절 되었어요.',
+  EXIT_VOTE:
+    '님에 대한 프로젝트 퇴출 투표가 생성되었어요. 24시간 내에 투표해주세요.',
+  VOTE_RESULT_EXIT_SUCCESS: '님이 프로젝트에서 퇴출 되었어요.',
+  VOTE_RESULT_EXIT_FAIL: '님의 퇴출 투표결과가 무효되었어요.',
+  DELETE_PROJECT: '에 팀장이 퇴출되어서 프로젝트가 삭제되었어요.',
+  REVIEW_REQUEST: '프로젝트는 어떠셨나요? 팀원 평가를 진행해주세요.',
+  REVIEW_RESULT: '프로젝트의 팀원 평가가 완료되었어요.',
+  FAVORITE_USER: '님이 새로운 프로젝트를 업로드하였어요.',
+  RECOMMEND: '프로젝트가 업데이트 되었어요.',
+}
+
+export const ALARM_BADGE = {
+  COMMENT: '댓글',
+  REPLY: '답글',
+  PROJECT_APPLY: '프로젝트',
+  APPLICANT_ACCEPT: '프로젝트',
+  APPLICANT_REJECT: '프로젝트',
+  EXIT_VOTE: '프로젝트',
+  VOTE_RESULT_EXIT_SUCCESS: '프로젝트',
+  VOTE_RESULT_EXIT_FAIL: '프로젝트',
+  DELETE_PROJECT: '프로젝트',
+  REVIEW_REQUEST: '프로젝트',
+  REVIEW_RESULT: '프로젝트',
+  FAVORITE_USER: '추천',
+  RECOMMEND: '추천',
+}

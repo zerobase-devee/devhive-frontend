6d02b1261243e40cfc19d055736dbcecab7f399e
Ahreum <cocoding420@gmail.com>
2023-09-27 00:15:46 +0900

    ✨ Feat : 모바일 메뉴 컴포넌트 추가

---
+++ src/components/common/header/ModileMenu.tsx
@@ -0,0 +1,95 @@
+import Link from 'next/link'
+import styles from './modileMenu.module.css'
+import { useRecoilValue } from 'recoil'
+import { loginState } from '@/recoil/loginState'
+import IsLoginButtons from './IsLoginButtons'
+import { IoMdClose } from 'react-icons/io'
+import { BiMenu } from 'react-icons/bi'
+import useModal from '@/hooks/useModal'
+import useLoginLogout from '@/utils/useLoginLogout'
+import Button from '../button/Button'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+const MobileMenu = () => {
+  const isLogin = useRecoilValue(loginState)
+  const { openModal, handleOpenModal, handleCloseModal } = useModal()
+  const { handleClick, onLogout } = useLoginLogout()
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      handleCloseModal()
+    }
+    router.events.on('routeChangeStart', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <div className={styles.container}>
+      {isLogin && !openModal && <IsLoginButtons />}
+      {openModal ? (
+        <button
+          className={styles.menuIcon}
+          type="button"
+          onClick={handleCloseModal}
+        >
+          <IoMdClose />
+        </button>
+      ) : (
+        <button
+          className={styles.menuIcon}
+          type="button"
+          onClick={handleOpenModal}
+        >
+          <BiMenu />
+        </button>
+      )}
+      {openModal && (
+        <div className={styles.menu}>
+          <nav className={styles.nav}>
+            <ul className={styles.navList}>
+              <li>
+                <Link href="/project">프로젝트</Link>
+              </li>
+              <li>
+                <Link href="/rank">랭킹</Link>
+              </li>
+              {isLogin && (
+                <>
+                  <li>
+                    <Link href={'/project/write'}>프로젝트올리기</Link>
+                  </li>
+                  <li>
+                    <Link href={'/mypage/myprofile'}>마이페이지</Link>
+                  </li>
+                </>
+              )}
+            </ul>
+            {!isLogin ? (
+              <Button type="button" onClick={handleClick} fill>
+                로그인
+              </Button>
+            ) : (
+              <Button
+                type="button"
+                onClick={() => {
+                  handleCloseModal()
+                  onLogout()
+                }}
+              >
+                로그아웃
+              </Button>
+            )}
+          </nav>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default MobileMenu

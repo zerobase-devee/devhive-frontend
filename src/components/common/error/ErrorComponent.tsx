06aa4db0d494eb692b9514e2333d531798f23571
Ahreum <cocoding420@gmail.com>
2023-10-12 01:30:20 +0900

    ✨ Feat :  className 추가

--- src/components/common/error/ErrorComponent.tsx
+++ src/components/common/error/ErrorComponent.tsx
@@ -6,7 +6,7 @@ const ErrorComponent = () => {
     <div className={styles.container}>
       <BiSolidMessageAltError />
       <p className={styles.title}>알 수 없는 문제가 발생했어요. :(</p>
-      <p>
+      <p className={styles.desc}>
         일시적인 현상이거나 인터넷 문제일 수 있으니, <br />
         새로고침 후 다시 시도해주세요.
       </p>

dbf3ab50d6d10f28163b2f5136930b5bab6657d7
Ahreum <cocoding420@gmail.com>
2023-11-07 20:34:37 +0900

    ✨ Feat :  마이페이지 레이아웃 반응형 추가

--- src/components/mypage/common/mypageLayout/MypageLayout.tsx
+++ src/components/mypage/common/mypageLayout/MypageLayout.tsx
@@ -1,16 +1,21 @@
 import styles from './mypageLayout.module.css'
 import MypageMenu from '../menu/MypageMenu'
 import Container from '@/components/common/container/Container'
+import useResponsiveSize from '@/hooks/useResponsiveSize'
 
 const MypageLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isSmallDeskTop } = useResponsiveSize()
+
   return (
     <Container>
-      <nav className={styles.nav}>
-        <p>마이페이지</p>
-        <ul className={styles.menu}>
-          <MypageMenu />
-        </ul>
-      </nav>
+      {!isSmallDeskTop && (
+        <nav className={styles.nav}>
+          <p>마이페이지</p>
+          <ul className={styles.menu}>
+            <MypageMenu />
+          </ul>
+        </nav>
+      )}
       <div className={styles.childrenContainer}>{children}</div>
     </Container>
   )

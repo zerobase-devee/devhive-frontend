d1d9d71721b2aa733efeb51c43fbf5c29bd1c0a9
Ahreum <cocoding420@gmail.com>
2024-02-17 13:47:31 +0900

    🔥 Remove : 필요없는 패키지 삭제 서버설정 주석처리

--- next.config.js
+++ next.config.js
@@ -2,28 +2,28 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  headers: [
-    {
-      source: '/api/:path*',
-      destination: 'https://devhive.store:8080/api/:path*',
-      headers: [
-        { key: 'Access-Control-Allow-Credentials', value: 'true' },
-        { key: 'Access-Control-Allow-Origin', value: '*' },
-        {
-          key: 'Access-Control-Allow-Methods',
-          value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-        },
-        {
-          key: 'Access-Control-Allow-Headers',
-          value:
-            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-        },
-      ],
-    },
-  ],
-  images: {
-    domains: ['devhive-image.s3.ap-northeast-2.amazonaws.com'],
-  },
+  // headers: [
+  //   {
+  //     source: '/api/:path*',
+  //     destination: 'https://devhive.store:8080/api/:path*',
+  //     headers: [
+  //       { key: 'Access-Control-Allow-Credentials', value: 'true' },
+  //       { key: 'Access-Control-Allow-Origin', value: '*' },
+  //       {
+  //         key: 'Access-Control-Allow-Methods',
+  //         value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+  //       },
+  //       {
+  //         key: 'Access-Control-Allow-Headers',
+  //         value:
+  //           'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  //       },
+  //     ],
+  //   },
+  // ],
+  // images: {
+  //   domains: ['devhive-image.s3.ap-northeast-2.amazonaws.com'],
+  // },
   webpack(config) {
     if (!config.resolve.fallback) {
       config.resolve.fallback = {}

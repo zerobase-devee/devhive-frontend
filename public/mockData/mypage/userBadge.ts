962c6ed56b178a6b61e82ba985fc9d602bf1c08f
Ahreum <cocoding420@gmail.com>
2024-02-19 21:09:58 +0900

    🤡 Mock: userMockdata 추가

---
+++ public/mockData/mypage/userBadge.ts
@@ -0,0 +1,48 @@
+import achievement1 from 'public/images/achievement/achievement1.png'
+import achievement2 from 'public/images/achievement/achievement2.png'
+import achievement3 from 'public/images/achievement/achievement3.png'
+import achievement4 from 'public/images/achievement/achievement4.png'
+import achievement5 from 'public/images/achievement/achievement5.png'
+
+export const userBadgeData = [
+  {
+    badgeDto: {
+      id: 0,
+      name: '매너왕',
+      image: achievement1,
+    },
+    score: 20,
+  },
+  {
+    badgeDto: {
+      id: 1,
+      name: '재능기부왕',
+      image: achievement2,
+    },
+    score: 40,
+  },
+  {
+    badgeDto: {
+      id: 2,
+      name: '소통왕',
+      image: achievement4,
+    },
+    score: 50,
+  },
+  {
+    badgeDto: {
+      id: 3,
+      name: '마감왕',
+      image: achievement3,
+    },
+    score: 60,
+  },
+  {
+    badgeDto: {
+      id: 4,
+      name: '척척박사',
+      image: achievement5,
+    },
+    score: 80,
+  },
+]
---
+++ public/mockData/mypage/userInfo.ts
@@ -0,0 +1,47 @@
+import techStackImg1 from '/public/images/techStack/javascript.png'
+import techStackImg2 from '/public/images/techStack/React.png'
+
+export const userProjectHistoriesData = [
+  {
+    projectName: '온라인 쇼핑몰',
+    totalAverageScore: '9',
+  },
+  {
+    projectName: '커피 쇼핑몰',
+    totalAverageScore: '8',
+  },
+  {
+    projectName: '개발자 서비스',
+    totalAverageScore: '10',
+  },
+]
+
+export const userCarrersData = [
+  {
+    carrerId: 1,
+    company: 'A회사',
+    position: '프론트엔드 개발자',
+    startDate: '2023-01-01T00:00:00',
+    endDate: null,
+  },
+  {
+    carrerId: 0,
+    company: 'B회사',
+    position: '프론트엔드 개발자',
+    startDate: '2022-01-01T00:00:00',
+    endDate: '2022-11-01T00:00:00',
+  },
+]
+
+export const userTechStacks = [
+  {
+    id: 0,
+    image: techStackImg1,
+    name: 'Javascript',
+  },
+  {
+    id: 1,
+    image: techStackImg2,
+    name: 'React',
+  },
+]

1844e1aef232273041a789a367a6fef22d3e2b73
Ahreum <cocoding420@gmail.com>
2024-02-19 17:49:36 +0900

    🤡 Mock: 이미지 mock 핸들러 추가

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,5 +1,11 @@
 import { postProjectList } from './project/postProjectList'
+import { imageUploadHandler } from './project/imageUpload'
 import { getRankList } from './rank/getRankList'
 import { loginHandler } from './auth/login'
 
-export const handlers = [postProjectList, getRankList, loginHandler]
+export const handlers = [
+  imageUploadHandler,
+  postProjectList,
+  getRankList,
+  loginHandler,
+]

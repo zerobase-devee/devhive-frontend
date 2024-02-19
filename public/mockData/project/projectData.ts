a110a713a5f04c4cdbcd878b1a4dd1516d081818
Ahreum <cocoding420@gmail.com>
2024-02-19 16:30:42 +0900

    🤡 Mock: 프로젝트 리스트 데이터 추가

---
+++ public/mockData/project/projectData.ts
@@ -0,0 +1,220 @@
+import { ProjectCardDataType } from '@/types/project/projectDataType'
+import techStackImg1 from '/public/images/techStack/javascript.png'
+import techStackImg2 from '/public/images/techStack/React.png'
+import techStackImg3 from '/public/images/techStack/typescript.png'
+
+export const projectData: ProjectCardDataType[] = [
+  {
+    bookmarkId: null,
+    createDate: '2024-02-19',
+    deadline: '2024-02-29',
+    developmentType: 'FRONTEND',
+    recruitmentType: 'ONLINE',
+    id: 0,
+    title: '캠핑 관련 사이드 프로젝트 팀원 모집합니다.',
+    name: '캠핑',
+    userNickname: '유저1',
+    profileImage: null,
+    status: 'RECRUITING',
+    region: null,
+    viewCount: 140,
+    techStackList: [
+      {
+        id: 0,
+        image: techStackImg1,
+        name: 'javascript',
+      },
+      {
+        id: 1,
+        image: techStackImg2,
+        name: 'React',
+      },
+      {
+        id: 2,
+        image: techStackImg3,
+        name: 'Typescript',
+      },
+    ],
+    projectMemberList: [
+      {
+        userId: 0,
+        nickName: '개발자',
+        profileImage: null,
+      },
+    ],
+  },
+  {
+    bookmarkId: null,
+    createDate: '2024-02-09',
+    deadline: '2024-02-29',
+    developmentType: 'BACKEND',
+    recruitmentType: 'ALL',
+    id: 1,
+    title: '반려동물 앱 관련 사이드 프로젝트 팀원 모집합니다.',
+    name: '반려동물 앱',
+    userNickname: '개발자',
+    profileImage: null,
+    status: 'RECRUITING',
+    region: '부산',
+    viewCount: 100,
+    techStackList: [
+      {
+        id: 2,
+        image: techStackImg3,
+        name: 'Typescript',
+      },
+    ],
+    projectMemberList: [
+      {
+        userId: 1,
+        nickName: '유저1',
+        profileImage: null,
+      },
+    ],
+  },
+  {
+    bookmarkId: null,
+    createDate: '2023-12-19',
+    deadline: '2024-02-29',
+    developmentType: 'FRONTEND',
+    recruitmentType: 'ONLINE',
+    id: 2,
+    title: '코딩 관련 사이드 프로젝트 팀원 모집합니다.',
+    name: '코딩',
+    userNickname: '유저1',
+    profileImage: null,
+    status: 'RECRUITING',
+    region: null,
+    viewCount: 50,
+    techStackList: [
+      {
+        id: 0,
+        image: techStackImg1,
+        name: 'javascript',
+      },
+    ],
+    projectMemberList: [
+      {
+        userId: 0,
+        nickName: '개발자',
+        profileImage: null,
+      },
+    ],
+  },
+  {
+    bookmarkId: null,
+    createDate: '2023-12-19',
+    deadline: '2024-02-29',
+    developmentType: 'FRONTEND',
+    recruitmentType: 'ONLINE',
+    id: 3,
+    title: '코딩 관련 사이드 프로젝트 팀원 모집합니다.',
+    name: '코딩',
+    userNickname: '유저1',
+    profileImage: null,
+    status: 'RECRUITING',
+    region: null,
+    viewCount: 50,
+    techStackList: [
+      {
+        id: 0,
+        image: techStackImg1,
+        name: 'javascript',
+      },
+    ],
+    projectMemberList: [
+      {
+        userId: 0,
+        nickName: '개발자',
+        profileImage: null,
+      },
+    ],
+  },
+  {
+    bookmarkId: null,
+    createDate: '2023-12-19',
+    deadline: '2024-02-29',
+    developmentType: 'FRONTEND',
+    recruitmentType: 'ONLINE',
+    id: 4,
+    title: '코딩 관련 사이드 프로젝트 팀원 모집합니다.',
+    name: '코딩',
+    userNickname: '유저1',
+    profileImage: null,
+    status: 'RECRUITING',
+    region: null,
+    viewCount: 50,
+    techStackList: [
+      {
+        id: 0,
+        image: techStackImg1,
+        name: 'javascript',
+      },
+    ],
+    projectMemberList: [
+      {
+        userId: 0,
+        nickName: '개발자',
+        profileImage: null,
+      },
+    ],
+  },
+  {
+    bookmarkId: null,
+    createDate: '2023-12-19',
+    deadline: '2024-02-29',
+    developmentType: 'FRONTEND',
+    recruitmentType: 'ONLINE',
+    id: 5,
+    title: '코딩 관련 사이드 프로젝트 팀원 모집합니다.',
+    name: '코딩',
+    userNickname: '유저1',
+    profileImage: null,
+    status: 'RECRUITING',
+    region: null,
+    viewCount: 50,
+    techStackList: [
+      {
+        id: 0,
+        image: techStackImg1,
+        name: 'javascript',
+      },
+    ],
+    projectMemberList: [
+      {
+        userId: 0,
+        nickName: '개발자',
+        profileImage: null,
+      },
+    ],
+  },
+  {
+    bookmarkId: null,
+    createDate: '2023-12-19',
+    deadline: '2024-02-29',
+    developmentType: 'FRONTEND',
+    recruitmentType: 'ONLINE',
+    id: 6,
+    title: '코딩 관련 사이드 프로젝트 팀원 모집합니다.',
+    name: '코딩',
+    userNickname: '유저1',
+    profileImage: null,
+    status: 'RECRUITING',
+    region: null,
+    viewCount: 50,
+    techStackList: [
+      {
+        id: 0,
+        image: techStackImg1,
+        name: 'javascript',
+      },
+    ],
+    projectMemberList: [
+      {
+        userId: 0,
+        nickName: '개발자',
+        profileImage: null,
+      },
+    ],
+  },
+]

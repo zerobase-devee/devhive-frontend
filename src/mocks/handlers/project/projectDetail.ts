a83904b3bdf6ad717aca39ef2563a17b6c8af309
Ahreum <cocoding420@gmail.com>
2024-03-10 00:00:14 +0900

    🤡 Mock: 로그인 전/후 디테일 페이지 데이터 수정

--- src/mocks/handlers/project/projectDetail.ts
+++ src/mocks/handlers/project/projectDetail.ts
@@ -1,9 +1,17 @@
 import { HttpResponse, http } from 'msw'
 import { commentMockData } from 'public/mockData/comment/commentData'
-import { projectDetialData } from 'public/mockData/project/projectDetail'
+import {
+  projectDetialData,
+  projectDetialLoginData,
+} from 'public/mockData/project/projectDetail'
 
 export const getProject = http.get('/api/projects/:projectId', () => {
-  const data = projectDetialData
+  const storedData = localStorage.getItem('loginState')
+  const isLogin = storedData
+    ? JSON.parse(storedData).loginState === true
+    : false
+
+  const data = isLogin ? projectDetialLoginData : projectDetialData
 
   return HttpResponse.json(data, { status: 200 })
 })

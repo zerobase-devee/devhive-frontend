562a9f991be1984674b98a97831a6804ce254875
Ahreum <cocoding420@gmail.com>
2024-03-09 23:14:29 +0900

    🐛 Fix : 북마크버튼 로그인 모달 추가, 북마크 등록 로직 주석처리

--- src/components/common/bookmarkButton/bookmarkButton.tsx
+++ src/components/common/bookmarkButton/bookmarkButton.tsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
 import styles from './bookmarkButton.module.css'
 import { BsFillBookmarkPlusFill } from 'react-icons/bs'
-import useFavorite from '@/hooks/queries/useFavorite'
+import InfoModal from '../modal/InfoModal'
+import { useRouter } from 'next/router'
+import { useRecoilValue } from 'recoil'
+import { loginState } from '@/recoil/loginState'
+// import useFavorite from '@/hooks/queries/useFavorite'
 
 const BookmarkButton = ({
   active,
@@ -17,47 +21,69 @@ const BookmarkButton = ({
   favoriteId?: number | null
 }) => {
   const [isFavorite, setIsFavorite] = useState(active)
+  const router = useRouter()
+  const isLogin = useRecoilValue(loginState)
+  const [showModal, setShowModal] = useState(false)
 
-  const {
-    addFavoriteUserMutation,
-    deleteFavoriteUserMutation,
-    addFavoriteProjectMutation,
-    deleteFavoriteProjectMutation,
-  } = useFavorite()
+  // const {
+  //   addFavoriteUserMutation,
+  //   deleteFavoriteUserMutation,
+  //   addFavoriteProjectMutation,
+  //   deleteFavoriteProjectMutation,
+  // } = useFavorite()
   const handleBookmark = async (event: React.MouseEvent<HTMLButtonElement>) => {
     try {
       event.preventDefault()
-      setIsFavorite(!isFavorite)
-      if (active === false) {
-        if (userId) {
-          await addFavoriteUserMutation.mutateAsync(userId)
-        } else if (projectId) {
-          await addFavoriteProjectMutation.mutateAsync(projectId)
-        }
-      } else {
-        if (favoriteId) {
-          await deleteFavoriteUserMutation.mutateAsync(favoriteId)
-        } else if (bookmarkId) {
-          await deleteFavoriteProjectMutation.mutateAsync(bookmarkId)
-        }
+      if (!isLogin) {
+        setShowModal(true)
+      } else if (active === false) {
+        setIsFavorite(!isFavorite)
+
+        // 북마크 등록 로직
+        //   if (userId) {
+        //     await addFavoriteUserMutation.mutateAsync(userId)
+        //   } else if (projectId) {
+        //     await addFavoriteProjectMutation.mutateAsync(projectId)
+        //   }
+        // } else {
+        //   if (favoriteId) {
+        //     await deleteFavoriteUserMutation.mutateAsync(favoriteId)
+        //   } else if (bookmarkId) {
+        //     await deleteFavoriteProjectMutation.mutateAsync(bookmarkId)
+        //   }
       }
     } catch (error) {
-      console.error(error)
+      console.error(error, ': API 요청 오류')
     }
   }
 
   return (
-    <button
-      type="button"
-      className={styles.bookmarkButton}
-      onClick={(event) => handleBookmark(event)}
-    >
-      <BsFillBookmarkPlusFill
-        className={`${styles.bookmarkIcon} ${
-          active === true && styles.favorite
-        }`}
-      />
-    </button>
+    <>
+      {showModal && (
+        <InfoModal
+          buttonText="로그인"
+          onClick={() => {
+            setShowModal(false)
+            router.push('/?user=login')
+          }}
+        >
+          로그인이 필요한 서비스입니다. <br />
+          로그인해주세요.
+        </InfoModal>
+      )}
+
+      <button
+        type="button"
+        className={styles.bookmarkButton}
+        onClick={(event) => handleBookmark(event)}
+      >
+        <BsFillBookmarkPlusFill
+          className={`${styles.bookmarkIcon} ${
+            isFavorite === true && styles.favorite
+          }`}
+        />
+      </button>
+    </>
   )
 }
 

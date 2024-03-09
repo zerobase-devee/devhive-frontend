2acc90724bb3beb9cd5ca850f39dcded8d19df6c
Ahreum <cocoding420@gmail.com>
2024-03-09 23:59:43 +0900

    🤡 Mock: 로그인 유저 데이터 추가

--- public/mockData/project/projectDetail.ts
+++ public/mockData/project/projectDetail.ts
@@ -54,3 +54,59 @@ export const projectDetialData: ProjectDetailDataType = {
   applyStatus: null,
   bookmarkId: null,
 }
+
+export const projectDetialLoginData: ProjectDetailDataType = {
+  status: 'RECRUITING',
+  projectTitle: '캠핑 관련 사이드 프로젝트 팀원 모집합니다.',
+  createDate: '2024-02-19',
+  modifiedDate: null,
+  viewCount: 150,
+  recruitmentType: 'ONLINE',
+  region: null,
+  developmentType: 'FRONTEND',
+  recruitmentNum: 4,
+  deadline: '2024-02-29',
+  projectName: '캠핑',
+  techStacks: [
+    {
+      id: 0,
+      image: techStackImg1,
+      name: 'Javascript',
+    },
+    {
+      id: 1,
+      image: techStackImg2,
+      name: 'React',
+    },
+    {
+      id: 2,
+      image: techStackImg3,
+      name: 'Typescript',
+    },
+  ],
+  content: '<p>같이 프로젝트 하실 팀원 구해요.</p>',
+  writerInfo: {
+    userId: 1,
+    nickName: '유저1',
+    profileImage: null,
+  },
+  projectMembers: [
+    {
+      userId: 1,
+      nickName: '유저1',
+      profileImage: null,
+    },
+    {
+      userId: 2,
+      nickName: '유저2',
+      profileImage: null,
+    },
+  ],
+  userInfo: {
+    userId: 999,
+    nickName: '개발자',
+    profileImage: null,
+  },
+  applyStatus: null,
+  bookmarkId: null,
+}
